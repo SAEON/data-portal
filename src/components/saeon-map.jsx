d249cb9cbadedc883b7b625c0f4b34286f1ac52e
Zach Smith <zachsmith.sa@gmail.com>
2019-08-01 17:07:44 +0200

    added a couple more layers

--- src/components/saeon-map.jsx
+++ src/components/saeon-map.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import Map from 'ol/Map'
 import View from 'ol/View'
-import TileLayer from 'ol/layer/Tile'
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js'
 import { Group } from 'ol/layer'
-import OSM from 'ol/source/OSM'
-import TileWMS from 'ol/source/TileWMS'
+import Point from 'ol/geom/Point.js'
 import { defaults as olControls } from 'ol/control.js'
 import { fromLonLat } from 'ol/proj.js'
 import { default as LayerSwitcher } from 'ol-layerswitcher'
-import { Tile, BingMaps } from 'ol/source';
+import Feature from 'ol/Feature.js'
+import { Tile, BingMaps, OSM, TileWMS, Cluster, Vector as VectorSource } from 'ol/source'
+import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style.js'
+
+// TEMP to remove once CORS is enabled
+import dotJson from './temp-dots.json'
+
+var styleCache = {}
+const getStyle = feature => {
+  var size = feature.get('features').length
+  var style = styleCache[size] || new Style({
+    image: new CircleStyle({
+      radius: 10,
+      stroke: new Stroke({
+        color: '#fff'
+      }),
+      fill: new Fill({
+        color: '#3399CC'
+      })
+    }),
+    text: new Text({
+      text: size.toString(),
+      fill: new Fill({
+        color: '#fff'
+      })
+    })
+  })
+  styleCache[size] = style
+  return style
+}
 
 export default class SaeonMap extends React.Component {
   constructor(props) {
@@ -16,7 +44,38 @@ export default class SaeonMap extends React.Component {
     this.map = null
     this.mapRef = React.createRef()
   }
-  componentDidMount() {
+
+  async componentDidMount() {
+    // let dots = null
+    // try {
+      // dots = await new Promise((resolve, reject) => {
+      //   fetch('http://app01.saeon.ac.za/nccrdapi/odata/Projects/GeoJson')
+      //     .then(result => result.text())
+      //     .then(text => resolve(text))
+      //     .catch(err => reject(err))
+      // })
+    // } catch (error) {
+    //   dots = 'error'
+    // }
+
+    // var count = 20000;
+    // var features = new Array(count);
+    // var e = 4500000;
+    // for (var i = 0; i < count; ++i) {
+    //   var coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
+    //   features[i] = new Feature(new Point(coordinates));
+    // }
+
+ 
+    const features = []
+    dotJson.forEach(f => {
+      const coordinates = f.geometry.coordinates
+      features.push(new Feature(new Point(fromLonLat(coordinates))))
+    })
+
+    console.log(features)
+    
+
     const layers = [
       new Group({
         title: 'BASES',
@@ -31,56 +90,35 @@ export default class SaeonMap extends React.Component {
           new TileLayer({
             title: 'OpenStreetMaps',
             source: new OSM({})
-          })
+          }),
+          new TileLayer({
+            title: 'CDNGI-Aerial',
+            visible: false,
+            source: new TileWMS({
+              url: 'http://aerial.openstreetmap.org.za/maps?map=/store/cd-ngi/cdngi-aerial.map&amp;',
+              params: {
+                'LAYERS': 'CDNGI-Aerial',
+                'TILED': true,
+                'FORMAT': 'image/png'
+              },
+              serverType: 'geoserver',
+              // Countries have transparency, so do not fade tiles:
+              transition: 0
+            }),
+            opacity: 1
+          }),          
         ]
       }),
       new Group({
         title: 'OVERLAYS',
-        layers: [
-          new Group({
-            title: 'BEEH',
-            layers: [
-              new TileLayer({
-                title: 'Stormflow',
-                visible: false,
-                source: new TileWMS({
-                  url: 'http://app01.saeon.ac.za:8082/geoserver/BEEH_shp/wms',
-                  params: {
-                    'LAYERS': 'BEEH_shp:hist_quickf.shp',
-                    'TILED': true,
-                    'FORMAT': 'image/png'
-                  },
-                  serverType: 'geoserver',
-                  // Countries have transparency, so do not fade tiles:
-                  transition: 0
-                }),
-                opacity: 0.7
-              }),
-              new TileLayer({
-                title: 'Stormflow (count of days)',
-                visible: false,
-                source: new TileWMS({
-                  url: 'http://app01.saeon.ac.za:8082/geoserver/BEEH_shp/wms',
-                  params: {
-                    'LAYERS': 'BEEH_shp:quickfgt2mm.shp',
-                    'TILED': true,
-                    'FORMAT': 'image/png'
-                  },
-                  serverType: 'geoserver',
-                  // Countries have transparency, so do not fade tiles:
-                  transition: 0
-                }),
-                opacity: 0.7
-              })
-            ]
-          }),      
+        layers: [     
           new TileLayer({
-            title: 'BEA:EXP_PP50',
+            title: 'Stormflow',
             visible: false,
             source: new TileWMS({
-              url: 'http://app01.saeon.ac.za:8086/geoserver/BEA/wms',
+              url: 'http://app01.saeon.ac.za:8082/geoserver/BEEH_shp/wms',
               params: {
-                'LAYERS': 'BEA:EXP_PP50',
+                'LAYERS': 'BEEH_shp:hist_quickf.shp',
                 'TILED': true,
                 'FORMAT': 'image/png'
               },
@@ -91,36 +129,31 @@ export default class SaeonMap extends React.Component {
             opacity: 0.7
           }),
           new TileLayer({
-            title: "DEA_CARBON",
+            title: 'Stormflow (count of days)',
             visible: false,
             source: new TileWMS({
-              url: 'http://app01.saeon.ac.za:8084/geoserver/DEA_CARBON_II/wms',
+              url: 'http://app01.saeon.ac.za:8082/geoserver/BEEH_shp/wms',
               params: {
-                'LAYERS': 'DEA_CARBON_II:Tmax A2 Change between 2000 and 2050 90th Percentile',
+                'LAYERS': 'BEEH_shp:quickfgt2mm.shp',
                 'TILED': true,
                 'FORMAT': 'image/png'
               },
               serverType: 'geoserver',
+              // Countries have transparency, so do not fade tiles:
               transition: 0
             }),
             opacity: 0.7
-          }),          
-          new TileLayer({
-            title: 'SUGAR',
-            visible: true,
-            source: new TileWMS({
-              url: 'http://app01.saeon.ac.za:8086/geoserver/BEA/wms',
-              params: {
-                'LAYERS': '=BEA:1_03_SUG',
-                'STYLE': '1_03_SUG_SUGAR',
-                'TILED': true,
-                'FORMAT': 'image/png'
-              },
-              serverType: 'geoserver',
-              transition: 0
+          }),
+          new VectorLayer({
+            title: 'Dots',
+            source: new Cluster({
+              distance: 50,
+              source: new VectorSource({
+                features: features
+              })
             }),
-            opacity: 0.7
-          })
+            style: feature => getStyle(feature)
+          })          
         ]
       })
     ]
