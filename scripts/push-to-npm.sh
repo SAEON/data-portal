35590bb3a0199e810dcc8970266556652ffb6708
Zach Smith <zachsmith.sa@gmail.com>
2019-11-04 11:56:43 +0200

    Committing current changes prior to minor version update

--- scripts/push-to-npm.sh
+++ scripts/push-to-npm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 npm init --scope=@saeon
-npm publish --access public
+npm publish --access public --force true
 
 echo "Complete! See your package at https://npmjs.com/package/@saeon/atlas"
