5174222637afd9d1f898635cc4bb2efa4c5ba925
Zach Smith <zachsmith.sa@gmail.com>
2020-01-16 15:41:54 +0200

    Updated publish scripts

--- scripts/increment-minor.sh
+++ scripts/increment-minor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
 git commit -m "Committing current changes prior to minor version update of NPM package"
+git push
 npm version minor -m "Increment package.json version (minor)"
