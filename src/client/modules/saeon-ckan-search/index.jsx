cbbf7363bf670c9a2838551fa712eff065363e27
Zach Smith <zachsmith.sa@gmail.com>
2020-01-22 16:49:37 +0200

    adjusted ckan explorer to narrow search by where wms server exists

--- src/client/modules/saeon-ckan-search/index.jsx
+++ src/client/modules/saeon-ckan-search/index.jsx
@@ -12,16 +12,16 @@ export default () => (
             const search = e.target.value
             updateForm(
               { search, loading: true },
-              debounceGlobal(async () => {
-                // Update CKAN search results
-                console.log('Updated search term', search)
-
-                const result = await fetch(
-                  `http://localhost:3000/saeon-metadata/search?index=saeon-odp-4-2&fields=metadata_json,record_id,organization&metadata_json.subjects.subject=${search},WMS"`
-                ).then(res => res.json())
-
-                updateForm({ result, loading: false })
-              }, 1000)
+              debounceGlobal(
+                async () =>
+                  updateForm({
+                    result: await fetch(
+                      `http://localhost:3000/saeon-metadata/search?index=saeon-odp-4-2&fields=metadata_json.linkedResources,record_id,metadata_json.titles,metadata_json.subjects&metadata_json.subjects.subject=${search}&metadata_json.linkedResources.resourceURL=*WMS`
+                    ).then(res => res.json()),
+                    loading: false
+                  }),
+                1000
+              )
             )
           }}
         />
@@ -32,30 +32,22 @@ export default () => (
             <div>
               <div>{fields.result.result_length || 0} items found</div>
               {fields.result.results
-                ? fields.result.results
-                    .filter(({ metadata_json }) =>
-                      metadata_json.subjects
-                        .reduce((acc, s) => [...new Set([...acc, s.subject.trim()])], [])
-                        .includes('WMS')
-                        ? true
-                        : false
-                    )
-                    .map(({ metadata_json }) =>
-                      metadata_json.linkedResources
-                        .filter(r => r.linkedResourceType === 'Query')
-                        .map((r, i) => (
-                          <div key={i}>
-                            <p style={{ display: 'inline-block' }}>{r.resourceDescription}</p>
+                ? fields.result.results.map(({ metadata_json }) =>
+                    metadata_json.linkedResources
+                      .filter(r => r.linkedResourceType === 'Query')
+                      .map((r, i) => (
+                        <div key={i}>
+                          <p style={{ display: 'inline-block' }}>{r.resourceDescription}</p>
 
-                            <input
-                              style={{ marginLeft: '8px' }}
-                              type="checkbox"
-                              checked={false}
-                              onChange={() => alert('hi')}
-                            />
-                          </div>
-                        ))
-                    )
+                          <input
+                            style={{ marginLeft: '8px' }}
+                            type="checkbox"
+                            checked={false}
+                            onChange={() => alert('hi')}
+                          />
+                        </div>
+                      ))
+                  )
                 : ''}
             </div>
           )}
