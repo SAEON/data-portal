2effdf4b522ef16a59f457a0e0facfae316d0912
Zach Smith <zachsmith.sa@gmail.com>
2020-01-22 12:57:15 +0200

    improved search to show layer names in a list of searching

--- src/client/modules/saeon-ckan-search/index.jsx
+++ src/client/modules/saeon-ckan-search/index.jsx
@@ -17,10 +17,8 @@ export default () => (
                 console.log('Updated search term', search)
 
                 const result = await fetch(
-                  `http://localhost:3000/saeon-metadata/search?index=saeon-odp-4-2&fields=metadata_json,record_id,organization&metadata_json.subjects.subject=${search}"`
-                )
-                  .then(res => res.text())
-                  .then(txt => JSON.parse(txt))
+                  `http://localhost:3000/saeon-metadata/search?index=saeon-odp-4-2&fields=metadata_json,record_id,organization&metadata_json.subjects.subject=${search},WMS"`
+                ).then(res => res.json())
 
                 updateForm({ result, loading: false })
               }, 1000)
@@ -28,7 +26,39 @@ export default () => (
           }}
         />
         <div>
-          {fields.loading ? 'Loading ...' : (fields.result.result_length || 0) + ' items found'}
+          {fields.loading ? (
+            'Loading ...'
+          ) : (
+            <div>
+              <div>{fields.result.result_length || 0} items found</div>
+              {fields.result.results
+                ? fields.result.results
+                    .filter(({ metadata_json }) =>
+                      metadata_json.subjects
+                        .reduce((acc, s) => [...new Set([...acc, s.subject.trim()])], [])
+                        .includes('WMS')
+                        ? true
+                        : false
+                    )
+                    .map(({ metadata_json }) =>
+                      metadata_json.linkedResources
+                        .filter(r => r.linkedResourceType === 'Query')
+                        .map((r, i) => (
+                          <div key={i}>
+                            <p style={{ display: 'inline-block' }}>{r.resourceDescription}</p>
+
+                            <input
+                              style={{ marginLeft: '8px' }}
+                              type="checkbox"
+                              checked={false}
+                              onChange={() => alert('hi')}
+                            />
+                          </div>
+                        ))
+                    )
+                : ''}
+            </div>
+          )}
         </div>
       </div>
     )}
