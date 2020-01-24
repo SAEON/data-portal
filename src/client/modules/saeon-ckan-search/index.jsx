ff3489a411939e898fe07e2cd4bd065a649309b8
Zach Smith <zachsmith.sa@gmail.com>
2020-01-24 16:07:13 +0200

    Updated ckan search mechanism to take into account pagination

--- src/client/modules/saeon-ckan-search/index.jsx
+++ src/client/modules/saeon-ckan-search/index.jsx
@@ -38,42 +38,76 @@ export default ({ proxy }) => (
             <div style={{ maxHeight: '300px', overflow: 'auto' }}>
               <div>{fields.searchResults[0]?.result_length || 0} items found</div>
               {fields.searchResults[0]?.results
-                ? fields.searchResults[0].results.map(({ metadata_json }) =>
-                    metadata_json.linkedResources
-                      .filter(r => r.linkedResourceType === 'Query')
-                      .map(({ resourceURL, resourceDescription }, i) => (
-                        <div key={i} style={{ margin: '8px 0', backgroundColor: '#8080807a' }}>
-                          <input
-                            style={{ margin: '0 8px' }}
-                            type="checkbox"
-                            checked={proxy.getLayerById(resourceDescription) || false}
-                            onChange={({ target }) => {
-                              if (target.checked) {
-                                const uri = npmUrl.parse(resourceURL, true)
-                                const { protocol, host, pathname, query } = uri
-                                const { layers } = query
-                                const serverAddress = `${protocol}//${host}${pathname}`
-                                proxy.addLayer(
-                                  newLayer({
-                                    id: resourceDescription,
-                                    title: resourceDescription,
-                                    url: serverAddress,
-                                    name: layers
-                                  })
-                                )
-                              } else {
-                                proxy.removeLayerById(resourceDescription)
-                              }
-                            }}
-                          />
-                          <p style={{ display: 'inline-block' }}>{resourceDescription}</p>
-                        </div>
-                      ))
-                  )
+                ? fields.searchResults
+                    .reduce(
+                      (result, current) => ({
+                        success: result.success && current.success,
+                        results: result.results.concat(current.results),
+                        result_length: result.result_length + current.result_length
+                      }),
+                      {
+                        success: fields.searchResults[0].success,
+                        result_length: 0,
+                        results: []
+                      }
+                    )
+                    .results.map(({ metadata_json }) =>
+                      metadata_json.linkedResources
+                        .filter(r => r.linkedResourceType === 'Query')
+                        .map(({ resourceURL, resourceDescription }, i) => {
+                          const uri = npmUrl.parse(resourceURL, true)
+                          const { protocol, host, pathname, query } = uri
+                          const { layers } = query
+                          const layerId = `${resourceDescription} - ${layers}`
+                          return (
+                            <div key={i} style={{ margin: '8px 0', backgroundColor: '#8080807a' }}>
+                              <input
+                                style={{ margin: '0 8px' }}
+                                type="checkbox"
+                                checked={proxy.getLayerById(layerId) || false}
+                                onChange={({ target }) => {
+                                  if (target.checked) {
+                                    const serverAddress = `${protocol}//${host}${pathname}`
+                                    proxy.addLayer(
+                                      newLayer({
+                                        id: layerId,
+                                        title: layerId,
+                                        url: serverAddress,
+                                        name: layers
+                                      })
+                                    )
+                                  } else {
+                                    proxy.removeLayerById(layerId)
+                                  }
+                                }}
+                              />
+                              <p style={{ display: 'inline-block' }}>{layerId}</p>
+                            </div>
+                          )
+                        })
+                    )
                 : ''}
               <div>
-                {fields.searchResults[0] && fields.searchResults[0].result_length >= 100 ? (
-                  <button onClick={() => alert('hi')}>Load more results</button>
+                {fields.searchResults[0]?.result_length >= 100 ? (
+                  <button
+                    onClick={async () =>
+                      updateForm({
+                        searchResults: [
+                          ...fields.searchResults,
+                          await searcher({
+                            url: `http://localhost:3000/saeon-metadata/search?index=saeon-odp-4-2&fields=metadata_json.linkedResources,record_id,metadata_json.titles,metadata_json.subjects&metadata_json.subjects.subject=${
+                              fields.search
+                            }&metadata_json.linkedResources.resourceURL=*WMS&start=${fields.searchResults.reduce(
+                              (c, curr) => c + curr.result_length,
+                              0
+                            ) + 1}`
+                          })
+                        ]
+                      })
+                    }
+                  >
+                    Load more results
+                  </button>
                 ) : (
                   ''
                 )}
