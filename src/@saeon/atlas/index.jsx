d98f347ba1751375c90bee20d0e02a52713cc9dd
Zach Smith <zachsmith.sa@gmail.com>
2020-01-27 13:05:25 +0200

    added roboto font

--- src/@saeon/atlas/index.jsx
+++ src/@saeon/atlas/index.jsx
@@ -1,5 +1,13 @@
+import './index.scss'
 import React from 'react'
 import { render } from 'react-dom'
+import WebFontLoader from 'webfontloader'
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons', 'Open+Sans:400,600']
+  }
+})
 
 const App = () => <div>hello world</div>
 
