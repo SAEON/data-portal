892931ddc9183618fee7e326b54df20aba0b9355
Zach Smith <zachsmith.sa@gmail.com>
2020-02-12 16:22:21 +0200

    moved publish scripts for ol-react to ol-react

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [['@babel/preset-env'], ['@babel/preset-react']],
   plugins: [
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-class-properties",
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-class-properties',
     [
-      "@babel/plugin-transform-runtime",
-      {"corejs": {"version": 3, "proposals": true}, "regenerator": true}
+      '@babel/plugin-transform-runtime',
+      { corejs: { version: 3, proposals: true }, regenerator: true }
     ]
   ]
-};
+}
