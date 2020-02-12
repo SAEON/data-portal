ae490746662e3c92987f4aacef6d20c2e58466f2
Zach Smith <zachsmith.sa@gmail.com>
2020-02-12 15:52:22 +0200

    Removed lerna, since all I was using it for was resolving a babel config in a parent directory. since babel now seems to allow for this .... I'm just using babel

---
+++ src/@saeon/ol-react/index.js
@@ -0,0 +1,8 @@
+// Export the OpenLayers API
+export { default } from './src/open-layers'
+
+// The custom Modules
+export { default as SingleFeatureSelector } from './src/modules/single-feature-selector'
+export { default as MapProxy } from './src/modules/map-proxy'
+export { default as DragAndDrop } from './src/modules/drag-and-drop'
+export { default as Draggable } from './src/modules/draggable'
--- src/@saeon/ol-react/src/index.jsx
+++
@@ -1,8 +0,0 @@
-// Export the OpenLayers API
-export { default } from './open-layers'
-
-// The custom Modules
-export { default as SingleFeatureSelector } from './modules/single-feature-selector'
-export { default as MapProxy } from './modules/map-proxy'
-export { default as DragAndDrop } from './modules/drag-and-drop'
-export { default as Draggable } from './modules/draggable'
