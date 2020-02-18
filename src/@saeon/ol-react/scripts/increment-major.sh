eda0a4e87c3bb989bfc0b953baf99677b6897929
Zach Smith <zachsmith.sa@gmail.com>
2020-02-18 16:25:11 +0200

    Committing current changes prior to patch version update of NPM package

--- src/@saeon/ol-react/scripts/increment-major.sh
+++ src/@saeon/ol-react/scripts/increment-major.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git add .
 git commit -m "Committing current changes prior to major version update of NPM package"
-git push
 npm version major -m "Increment package.json version (major)"
+git add .
+git commit -m "Bumped patch"
+git push
