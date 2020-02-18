90378f9aef53333d9bf406ad6b368a1665739a40
Zach Smith <zachsmith.sa@gmail.com>
2020-02-18 11:28:14 +0200

    Updated API to log allowed origins on startup, and updated API configuration on deployment. Also updated API babel config to be the main config

--- src/@saeon/atlas-api/src/app.js
+++ src/@saeon/atlas-api/src/app.js
@@ -32,20 +32,23 @@ const asyncHandler = fn => (req, res, next) =>
     return next()
   })
 
-const corsMiddleware = (ALLOWED_ORIGINS => (req, res, next) => {
-  const { method, headers } = req
-  const { origin } = headers
-  console.log(`Checking CORS policy`, `${origin}`, `${ALLOWED_ORIGINS.includes(origin)}`)
-  if (ALLOWED_ORIGINS.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin)
-  }
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  res.header('Access-Control-Allow-Credentials', true)
-  if (method === 'OPTIONS') {
-    res.sendStatus(200)
-  } else {
-    next()
+const corsMiddleware = (ALLOWED_ORIGINS => {
+  console.log('Allowed origins registered', ALLOWED_ORIGINS)
+  return (req, res, next) => {
+    const { method, headers } = req
+    const { origin } = headers
+    console.log(`Checking CORS policy`, `${origin}`, `${ALLOWED_ORIGINS.includes(origin)}`)
+    if (ALLOWED_ORIGINS.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin)
+    }
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    res.header('Access-Control-Allow-Credentials', true)
+    if (method === 'OPTIONS') {
+      res.sendStatus(200)
+    } else {
+      next()
+    }
   }
 })(
   process.env.ALLOWED_ORIGINS
