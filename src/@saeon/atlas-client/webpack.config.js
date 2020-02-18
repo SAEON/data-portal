a8dde4e2abf9a5c13d72ed0921bb82b297fcc2d7
Zach Smith <zachsmith.sa@gmail.com>
2020-02-18 14:47:29 +0200

    Added back linting tooling

--- src/@saeon/atlas-client/webpack.config.js
+++ src/@saeon/atlas-client/webpack.config.js
@@ -1,8 +1,8 @@
 const Dotenv = require('dotenv-webpack')
 require('dotenv').config()
 module.exports = require('../../../webpack.config')({
-  pluginsCb: (plugins, mode) => [...plugins, new Dotenv()],
-  resolveCb: mode => ({
+  pluginsCb: (plugins) => [...plugins, new Dotenv()],
+  resolveCb: () => ({
     alias: {
       "@saeon/ol-react": "../../../../ol-react/index"
     }
