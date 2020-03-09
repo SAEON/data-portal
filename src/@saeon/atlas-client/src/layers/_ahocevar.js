5d8ffa8347e06bfd97dbadb4b3e4e28751154881
Zach Smith <zachsmith.sa@gmail.com>
2020-03-09 12:44:14 +0200

    Updated ahocevar wms layer to be tiled

--- src/@saeon/atlas-client/src/layers/_ahocevar.js
+++ src/@saeon/atlas-client/src/layers/_ahocevar.js
@@ -1,6 +1,7 @@
 import { Tile as TileLayer } from 'ol/layer.js'
 import { TileWMS } from 'ol/source'
 
+// https://ahocevar.com/geoserver/wms?SERVICE=WMS&VERSION=1.3.0&REQUEST=getCapabilities
 export const ahocevarBaseMap = () =>
   new TileLayer({
     title: 'Ahocevar Base Map',
