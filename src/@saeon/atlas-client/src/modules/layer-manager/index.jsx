21732916e85918096b529d87c28e9b5c420300ad
Zach Smith <zachsmith.sa@gmail.com>
2020-03-16 12:11:58 +0200

    Updated layer-manager to show controls that will be useful in controlling layers

--- src/@saeon/atlas-client/src/modules/layer-manager/index.jsx
+++ src/@saeon/atlas-client/src/modules/layer-manager/index.jsx
@@ -5,7 +5,9 @@ import {
   Delete,
   ExpandLess,
   ExpandMore,
-  DragIndicator
+  DragIndicator,
+  Info as InfoIcon,
+  VpnKey
 } from '@material-ui/icons'
 import {
   Card,
@@ -16,10 +18,16 @@ import {
   Typography,
   IconButton,
   Collapse,
-  Slider
+  Slider,
+  Button
 } from '@material-ui/core'
 import { Form, DragAndDrop } from '../../components'
 import { debounce } from '../../../../fns-lib'
+
+const headerButtonProps = {
+  color: 'inherit',
+  size: 'small'
+}
 
 export default class extends Component {
   state = {
@@ -66,6 +74,7 @@ export default class extends Component {
                     }}
                     variant="outlined"
                   >
+                    {/* Layer item header */}
                     <CardHeader
                       component={({ children }) => (
                         <AppBar position="relative" variant="outlined">
@@ -73,28 +82,37 @@ export default class extends Component {
                             style={{ paddingRight: 0, paddingLeft: 0 }}
                             className="thin-toolbar"
                           >
+                            {/* Drag icon */}
                             <DragIndicator style={{ marginRight: 10 }} />
+
+                            {/* Title (comes from CardHeader.title prop) */}
                             {children}
+
+                            {/* Epand layer info button */}
                             <IconButton
-                              size="small"
+                              {...headerButtonProps}
                               onClick={() =>
                                 this.setState({
                                   [layer.get('id')]: state[layer.get('id')] ? false : true
                                 })
                               }
-                              aria-label="toggle-layer-info"
+                              aria-label="expand-item-card"
                             >
                               {state[layer.get('id')] ? <ExpandLess /> : <ExpandMore />}
                             </IconButton>
+
+                            {/* Toggle layer visibility icon */}
                             <IconButton
-                              size="small"
+                              {...headerButtonProps}
                               onClick={() => layer.setVisible(!layer.get('visible'))}
                               aria-label="toggle-visibility"
                             >
                               {layer.get('visible') ? <Visibility /> : <VisibilityOff />}
                             </IconButton>
+
+                            {/* Delete layer icon */}
                             <IconButton
-                              size="small"
+                              {...headerButtonProps}
                               onClick={() => proxy.removeLayer(layer)}
                               aria-label="delete-layer"
                               style={{ marginRight: 10 }}
@@ -112,27 +130,51 @@ export default class extends Component {
                     />
                     <Collapse in={state[layer.get('id')]} timeout="auto" unmountOnExit>
                       <CardContent>
+                        <Button
+                          className="layer-info-control"
+                          variant="outlined"
+                          startIcon={<VpnKey />}
+                          onClick={() => alert('hi')}
+                          size="small"
+                        >
+                          Legend
+                        </Button>
+                        <Button
+                          className="layer-info-control"
+                          variant="outlined"
+                          startIcon={<InfoIcon />}
+                          onClick={() => alert('hi')}
+                          size="small"
+                        >
+                          Layer info
+                        </Button>
                         <Form value={layer.get('opacity') * 100}>
                           {({ updateForm, value }) => (
-                            <Slider
-                              onMouseEnter={() => this.setState({ disableDrag: true })}
-                              onMouseLeave={() => this.setState({ disableDrag: false })}
-                              defaultValue={50}
-                              getAriaValueText={() => parseInt(value, 10)}
-                              value={value}
-                              onChange={(e, v) =>
-                                updateForm(
-                                  { value: v },
-                                  debounce(() => layer.setOpacity(v / 100))
-                                )
-                              }
-                              aria-labelledby="discrete-slider-small-steps"
-                              step={0.00001}
-                              marks={false}
-                              min={1}
-                              max={100}
-                              valueLabelDisplay="off"
-                            />
+                            <div className="layer-info-control">
+                              <Typography style={{ display: 'table-cell', paddingRight: 20 }}>
+                                Opacity
+                              </Typography>
+                              <Slider
+                                style={{ display: 'table-cell' }}
+                                onMouseEnter={() => this.setState({ disableDrag: true })}
+                                onMouseLeave={() => this.setState({ disableDrag: false })}
+                                defaultValue={50}
+                                getAriaValueText={() => parseInt(value, 10)}
+                                value={value}
+                                onChange={(e, v) =>
+                                  updateForm(
+                                    { value: v },
+                                    debounce(() => layer.setOpacity(v / 100))
+                                  )
+                                }
+                                aria-labelledby="discrete-slider-small-steps"
+                                step={0.00001}
+                                marks={false}
+                                min={1}
+                                max={100}
+                                valueLabelDisplay="off"
+                              />
+                            </div>
                           )}
                         </Form>
                       </CardContent>
