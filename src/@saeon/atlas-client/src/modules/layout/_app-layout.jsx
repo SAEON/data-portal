de65935f8479b0849edc7727fa79ebc23da82940
Zach Smith <zachsmith.sa@gmail.com>
2020-03-17 12:46:00 +0200

    Refactored the navigation components away, since they didn't make code clearer. This is inline with handling the legend and layer info menus better

--- src/@saeon/atlas-client/src/modules/layout/_app-layout.jsx
+++ src/@saeon/atlas-client/src/modules/layout/_app-layout.jsx
@@ -9,7 +9,6 @@ import {
   Avatar,
   Fab
 } from '@material-ui/core'
-import { NavigationDial, navItem } from './_navigation'
 import SaeonSearch from '../saeon-search'
 import LayerManager from '../layer-manager'
 import CsirLayers from '../csir-layers'
@@ -21,7 +20,7 @@ import {
   Menu as MenuIcon,
   GitHub as GitHubIcon
 } from '@material-ui/icons'
-import { DragMenu, SideMenu } from '../../components'
+import { DragMenu, SideMenu, SpeedDial, Form } from '../../components'
 import { MenuContext } from '../menu-provider'
 
 export default ({ proxy }) => (
@@ -74,83 +73,102 @@ export default ({ proxy }) => (
         </AppBar>
 
         {/* Search SpeedDial menu */}
-        <NavigationDial
-          direction={'left'}
-          icon={<SearchIcon />}
-          style={{ position: 'absolute', right: 20, top: 57 }}
-        >
-          {() => [
-            navItem({
-              icon: <Avatar>S</Avatar>,
-              tooltip: 'Saerch SAEON data',
-              toggle: () =>
-                updateMenuManager({ saeonSearchMenu: { active: !fields.saeonSearchMenu.active } }),
-              title: 'Search SAEON data',
-              active: fields.saeonSearchMenu.active,
-              Component: (
-                <DragMenu
-                  onMouseDown={() => setActiveMenu('saeonSearchMenu')}
-                  zIndex={fields.saeonSearchMenu.zIndex}
-                  title={'Search SAEON data'}
-                  active={fields.saeonSearchMenu.active}
-                  close={() => updateMenuManager({ saeonSearchMenu: { active: false } })}
-                >
-                  <SaeonSearch proxy={proxy} />
-                </DragMenu>
-              )
-            }),
-            navItem({
-              icon: <Avatar>C</Avatar>,
-              tooltip: 'Saerch CSIR data',
-              toggle: () =>
-                updateMenuManager({ csirSearchMenu: { active: !fields.csirSearchMenu.active } }),
-              title: 'Search CSIR data',
-              active: fields.csirSearchMenu.active,
-              Component: (
-                <SideMenu
-                  title={'Search CSIR data'}
-                  location="top"
-                  width={420}
-                  height={'calc(100% - 75px)'}
-                  active={fields.csirSearchMenu.active}
-                  toggle={() =>
-                    updateMenuManager({ csirSearchMenu: { active: !fields.csirSearchMenu.active } })
-                  }
-                >
-                  <CsirLayers proxy={proxy} />
-                </SideMenu>
-              )
-            })
-          ]}
-        </NavigationDial>
+        <Form dialOpen={false}>
+          {({ updateForm, dialOpen }) => (
+            <SpeedDial
+              style={{ position: 'absolute', right: 20, top: 57 }}
+              direction={'left'}
+              onOpen={() => updateForm({ dialOpen: true })}
+              onClose={() => updateForm({ dialOpen: false })}
+              open={dialOpen}
+              icon={<SearchIcon />}
+            >
+              {[
+                {
+                  icon: <Avatar>S</Avatar>,
+                  tooltip: 'Saerch SAEON data',
+                  toggle: () =>
+                    updateMenuManager({
+                      saeonSearchMenu: { active: !fields.saeonSearchMenu.active }
+                    }),
+                  title: 'Search SAEON data',
+                  active: fields.saeonSearchMenu.active,
+                  Component: (
+                    <DragMenu
+                      onMouseDown={() => setActiveMenu('saeonSearchMenu')}
+                      zIndex={fields.saeonSearchMenu.zIndex}
+                      title={'Search SAEON data'}
+                      active={fields.saeonSearchMenu.active}
+                      close={() => updateMenuManager({ saeonSearchMenu: { active: false } })}
+                    >
+                      <SaeonSearch proxy={proxy} />
+                    </DragMenu>
+                  )
+                },
+                {
+                  icon: <Avatar>C</Avatar>,
+                  tooltip: 'Saerch CSIR data',
+                  toggle: () =>
+                    updateMenuManager({
+                      csirSearchMenu: { active: !fields.csirSearchMenu.active }
+                    }),
+                  title: 'Search CSIR data',
+                  active: fields.csirSearchMenu.active,
+                  Component: (
+                    <SideMenu
+                      title={'Search CSIR data'}
+                      location="top"
+                      width={420}
+                      height={'calc(100% - 75px)'}
+                      active={fields.csirSearchMenu.active}
+                      toggle={() =>
+                        updateMenuManager({
+                          csirSearchMenu: { active: !fields.csirSearchMenu.active }
+                        })
+                      }
+                    >
+                      <CsirLayers proxy={proxy} />
+                    </SideMenu>
+                  )
+                }
+              ]}
+            </SpeedDial>
+          )}
+        </Form>
 
         {/* Layers SpeedDial menu */}
-        <NavigationDial
-          direction={'left'}
-          icon={<LayersIcon />}
-          chartMenuOpen={false}
-          style={{ position: 'absolute', right: 20, top: 127 }}
-        >
-          {() => [
-            navItem({
-              icon: <ListIcon />,
-              tooltip: 'Layers menu',
-              toggle: () =>
-                updateMenuManager({ layersMenu: { active: !fields.layersMenu.active } }),
-              Component: (
-                <DragMenu
-                  onMouseDown={() => setActiveMenu('layersMenu')}
-                  zIndex={fields.layersMenu.zIndex}
-                  title={'Open layers menu'}
-                  active={fields.layersMenu.active}
-                  close={() => updateMenuManager({ layersMenu: { active: false } })}
-                >
-                  <LayerManager layersActive={fields.layersMenu.active} proxy={proxy} />
-                </DragMenu>
-              )
-            })
-          ]}
-        </NavigationDial>
+        <Form dialOpen={false}>
+          {({ updateForm, dialOpen }) => (
+            <SpeedDial
+              style={{ position: 'absolute', right: 20, top: 127 }}
+              direction={'left'}
+              onOpen={() => updateForm({ dialOpen: true })}
+              onClose={() => updateForm({ dialOpen: false })}
+              open={dialOpen}
+              icon={<LayersIcon />}
+            >
+              {[
+                {
+                  icon: <ListIcon />,
+                  tooltip: 'Layers menu',
+                  toggle: () =>
+                    updateMenuManager({ layersMenu: { active: !fields.layersMenu.active } }),
+                  Component: (
+                    <DragMenu
+                      onMouseDown={() => setActiveMenu('layersMenu')}
+                      zIndex={fields.layersMenu.zIndex}
+                      title={'Open layers menu'}
+                      active={fields.layersMenu.active}
+                      close={() => updateMenuManager({ layersMenu: { active: false } })}
+                    >
+                      <LayerManager layersActive={fields.layersMenu.active} proxy={proxy} />
+                    </DragMenu>
+                  )
+                }
+              ]}
+            </SpeedDial>
+          )}
+        </Form>
 
         {/* Config menu */}
         <>
