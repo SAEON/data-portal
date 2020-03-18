2fd39b44864cbf8040c01b81367e49d4ad0de270
Zach Smith <zachsmith.sa@gmail.com>
2020-03-18 16:20:15 +0200

    Added json tree viewer for the info panel

--- src/@saeon/atlas-client/src/modules/layer-manager/_info.jsx
+++ src/@saeon/atlas-client/src/modules/layer-manager/_info.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { DragMenu } from '../../components'
+import JSONTree from 'react-json-tree'
 import { Typography } from '@material-ui/core'
 
 const ATLAS_API_ADDRESS = process.env.ATLAS_API_ADDRESS || 'http://localhost:4000'
@@ -42,7 +43,30 @@ export default ({ title, uri, onClose }) => {
           <div style={{ marginBottom: 10 }}>
             <Typography variant="overline">{title}</Typography>
           </div>
-          {JSON.stringify(state.data)}
+          <JSONTree
+            invertTheme={true}
+            theme={{
+              scheme: 'pop',
+              author: 'chris kempson (http://chriskempson.com)',
+              base00: '#000000',
+              base01: '#202020',
+              base02: '#303030',
+              base03: '#505050',
+              base04: '#b0b0b0',
+              base05: '#d0d0d0',
+              base06: '#e0e0e0',
+              base07: '#ffffff',
+              base08: '#eb008a',
+              base09: '#f29333',
+              base0A: '#f8ca12',
+              base0B: '#37b349',
+              base0C: '#00aabb',
+              base0D: '#0e5a94',
+              base0E: '#b31e8d',
+              base0F: '#7a2d00'
+            }}
+            data={state.data}
+          />
         </>
       )}
     </DragMenu>
