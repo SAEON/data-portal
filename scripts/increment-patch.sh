2d2f552cb8d1621077ec7f72f5a972ffce4dc9bd
Zach Smith <zachsmith.sa@gmail.com>
2020-03-25 10:13:45 +0200

    fixed flag

--- scripts/increment-patch.sh
+++ scripts/increment-patch.sh
@@ -3,5 +3,5 @@ git add .
 git commit -m "Committing current changes prior to patch version update of NPM package" --no-verify
 npm version patch -m "Increment package.json version (patch)"
 git add .
-git commit -m "Bumped patch" ---no-verify
+git commit -m "Bumped patch" --no-verify
 git push
