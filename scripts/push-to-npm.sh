a8a660e6c3e07bf3e2ccf40b82de6fda33a24464
Zach Smith <zachsmith.sa@gmail.com>
2020-04-01 16:12:30 +0200

    Updated npm publish scripts to specify package name on console print

--- scripts/push-to-npm.sh
+++ scripts/push-to-npm.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+while getopts u:d:p:f: option
+do
+case "${option}"
+in
+p) PACKAGE=${OPTARG};;
+esac
+done
+
 npm init --scope=@saeon
 npm publish --access public
 
-echo "Complete! See your package at https://npmjs.com/package/@saeon/<package-name>"
+echo "Complete! See your package at https://npmjs.com/package/$PACKAGE"
