1c37aa95ceb7d9c189b8a22147230db57d513447
Zach Smith <zachsmith.sa@gmail.com>
2020-04-03 09:22:28 +0200

    Finished removing references to a single webpack file. was more complicated than having a small amount of code repeated

--- src/@saeon/ol-react/webpack.config.js
+++ src/@saeon/ol-react/webpack.config.js
@@ -1,21 +1,58 @@
 const path = require('path')
 
-module.exports = require(path.resolve(__dirname, '../../../webpack.config'))({
-  pluginsCb: (plugins, mode) => (mode === 'production' ? [] : plugins),
-  outputCb: ({ mode, output }) =>
-    mode === 'production'
-      ? {
-          filename: 'index.js',
-          libraryTarget: 'commonjs2',
-          path: output,
-        }
-      : {
-          filename: 'index.js',
+module.exports = ({ mode, entry, output = '/dist' }) => ({
+  mode,
+  entry: path.resolve(__dirname, entry),
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, output),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    react: 'react',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            rootMode: 'upward',
+          },
         },
-  externalsCb: (mode) =>
-    mode === 'production'
-      ? {
-          react: 'react',
-        }
-      : {},
+      },
+      {
+        test: /\.*css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              fallback: 'file-loader',
+              name: '[name][md5:hash].[ext]',
+              outputPath: 'assets/',
+              publicPath: '/assets/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [],
 })
