5a55d403eb3aacc18d60c3a847358d401fda450d
Zach Smith <zachsmith.sa@gmail.com>
2020-04-09 14:29:59 +0200

    Updated ol-react externals

--- src/@saeon/ol-react/webpack.config.js
+++ src/@saeon/ol-react/webpack.config.js
@@ -13,8 +13,7 @@ module.exports = ({ mode, entry, output = '/dist' }) => ({
   },
   externals: {
     react: 'react',
-    'ol/Map': 'ol/Map',
-    'ol/View': 'ol/View',
+    'ol': 'ol',
     'ol/control.js': 'ol/control.js',
     'ol/layer/Group': 'ol/layer/Group',
     'ol/format': 'ol/format',
