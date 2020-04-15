252258319a6c1780573ce931552fc213aceee421
Zach Smith <zachsmith.sa@gmail.com>
2020-04-15 13:46:42 +0200

    fixed ref problemee that was stopping menu row highlight in the top bar

--- src/@saeon/atlas-client/src/modules/layout/nav-item.jsx
+++ src/@saeon/atlas-client/src/modules/layout/nav-item.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import { Link, Route } from 'react-router-dom'
 import { MenuItem, ListItemIcon, ListItemText } from '@material-ui/core'
 
-export default ({ label, to, icon, exact = true, href }) => (
+export default forwardRef(({ label, to, icon, exact = true, href }, ref) => (
   <Route path={to} exact={exact}>
-    {({ match }) => (
-      <MenuItem
-        component={href ? 'a' : Link}
-        active={match && !href}
-        rel={href && 'noopener noreferrer'}
-        target={href && '_blank'}
-        to={to}
-        href={href}
-      >
-        <ListItemIcon>{icon}</ListItemIcon>
-        <ListItemText primary={label} />
-      </MenuItem>
-    )}
+    {({ match }) => {
+      return (
+        <MenuItem
+          innerRef={ref}
+          component={href ? 'a' : Link}
+          active={match}
+          rel={href && 'noopener noreferrer'}
+          target={href && '_blank'}
+          to={to}
+          href={href}
+        >
+          <ListItemIcon>{icon}</ListItemIcon>
+          <ListItemText primary={label} />
+        </MenuItem>
+      )
+    }}
   </Route>
-)
+))
