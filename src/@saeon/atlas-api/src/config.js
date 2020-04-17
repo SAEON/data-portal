11b439277eae25be0116e4a8e9b8f9fc63f1ebc9
Zach Smith <zachsmith.sa@gmail.com>
2020-04-17 12:04:15 +0200

    Updated configuration logging

--- src/@saeon/atlas-api/src/config.js
+++ src/@saeon/atlas-api/src/config.js
@@ -3,8 +3,14 @@ config()
 
 export const PORT = process.env.PORT || 3000
 export const NODE_ENV = process.env.NODE_ENV || 'development'
-
 export const HTTP_PROXY = process.env.HTTP_PROXY || 'http://localhost:8001'
-
 export const ALLOWED_ORIGINS =
   process.env.ALLOWED_ORIGINS || 'http://localhost:3000,http://localhost:3001'
+
+console.log(
+  'Configuration',
+  `PORT: ${PORT}`,
+  `NODE_ENV: ${NODE_ENV}`,
+  `HTTP_PROXY: ${HTTP_PROXY}`,
+  `ALLOWED_ORIGINS: ${ALLOWED_ORIGINS}`
+)
