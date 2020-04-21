6dc2f502474e33249dbb6ad7021b5837a6f86676
Zach Smith <zach@saeon.ac.za>
2020-04-21 10:05:19 +0200

    Deleted a superfluous folder that magically reappeared

--- src/@saeon/atlas-client/src/modules/page-atlas/index.jsx
+++ src/@saeon/atlas-client/src/modules/page-atlas/index.jsx
@@ -4,6 +4,7 @@ import SearchMenu from './_search-menu'
 import MapMenu from './_map-menu'
 import AboutMenu from './_about-menu'
 import { Fade } from '@material-ui/core'
+// import ScreenshotMenu from './_screenshot-menu'
 
 export default () => (
   <Fade in={true}>
@@ -11,6 +12,7 @@ export default () => (
       <SearchMenu />
       <MapMenu />
       <AboutMenu />
+      {/* <ScreenshotMenu/> */}
     </>
   </Fade>
 )
