3c13cf82165037afbb8e4cddde99adba59afba87
Zach Smith <zach@saeon.ac.za>
2020-04-28 09:56:05 +0200

    Added webpack configuration for snap menus

---
+++ src/@saeon/snap-menus/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+module.exports = ({ mode, entry, output = '/dist' }) => ({
+  mode,
+  entry: path.resolve(__dirname, entry),
+  output: {
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+    path: path.join(__dirname, output),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  externals: {
+    react: 'react',
+    'react-md': 'react-md',
+    '@material-ui/core': '@material-ui/core',
+    '@material-ui/icons': '@material-ui/icons'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            rootMode: 'upward',
+          },
+        },
+      },
+      {
+        test: /\.*css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [],
+})
