c2b3425b93d1d24706f393e396bfb5e4f8917ed6
Zach Smith <zach@saeon.ac.za>
2020-04-29 09:16:26 +0200

    Fixed proxy path error for the app01-addressed geoservers

--- src/@saeon/anyproxy/src/rules/_saeon-geoserver-1.js
+++ src/@saeon/anyproxy/src/rules/_saeon-geoserver-1.js
@@ -5,7 +5,7 @@ const SAEON_SPATIALDATA_PROXY_PARSED = url.parse(SAEON_SPATIALDATA_PROXY)
 
 export default ({ path, requestDetail }) => {
   const { protocol, hostname, host, path: proxyPath } = SAEON_SPATIALDATA_PROXY_PARSED
-  const port = path.match(/^\/saeon-spatialdata\/app01.saeon.ac.za\/\d{4}/)[0].slice(-4)
+  const port = path.match(/^\/proxy\/saeon-spatialdata\/app01.saeon.ac.za\/\d{4}/)[0].slice(-4)
   requestDetail.protocol = protocol
   return {
     headers: Object.assign(requestDetail.requestOptions.headers, { host }),
--- src/@saeon/anyproxy/src/rules/_saeon-geoserver-2.js
+++ src/@saeon/anyproxy/src/rules/_saeon-geoserver-2.js
@@ -4,8 +4,8 @@ import { SAEON_SPATIALDATA_PROXY2 } from '../config'
 const SAEON_SPATIALDATA_PROXY2_PARSED = url.parse(SAEON_SPATIALDATA_PROXY2)
 
 export default ({ path, requestDetail }) => {
-  const { protocol, hostname, host, proxy: proxyPath } = SAEON_SPATIALDATA_PROXY2_PARSED
-  const port = path.match(/^\/saeon-spatialdata\/196\.21\.191\.55\/\d{4}/)[0].slice(-4)
+  const { protocol, hostname, host, path: proxyPath } = SAEON_SPATIALDATA_PROXY2_PARSED
+  const port = path.match(/^\/proxy\/saeon-spatialdata\/196\.21\.191\.55\/\d{4}/)[0].slice(-4)
   requestDetail.protocol = protocol
   return {
     headers: Object.assign(requestDetail.requestOptions.headers, { host }),
