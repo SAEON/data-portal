94b61aafca17b1832bb7668cfa7854d7b44e7156
Zach Smith <zach@saeon.ac.za>
2020-05-08 11:22:38 +0200

    bug fix and repush

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   globals: {
+    globalThis: false,
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
