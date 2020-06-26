2d3546ff80b7d94adcc2067e8ef2c3f33d0214df
Zach Smith <zach@saeon.ac.za>
2020-06-26 12:23:54 +0200

    Staging current changes. Bumping patch (@saeon/quick-form)

---
+++ src/packages/quick-form/src/index.js
@@ -0,0 +1,31 @@
+import { useState, useEffect, memo } from 'react'
+
+export default memo(props => {
+  const [fields, updateAllFields] = useState(
+    Object.fromEntries(
+      Object.entries(props).filter(
+        ([key]) =>
+          Object.prototype.hasOwnProperty.call(props, key) &&
+          key !== 'children' &&
+          key !== 'effects'
+      )
+    )
+  )
+
+  useEffect(
+    () =>
+      props.effects?.forEach(effect => {
+        effect(fields)
+      }),
+    Object.entries(fields).map(([, v]) => v)
+  )
+
+  const updateForm = obj => {
+    updateAllFields(Object.assign({ ...fields }, obj))
+  }
+
+  return props.children({
+    updateForm,
+    ...fields,
+  })
+})
