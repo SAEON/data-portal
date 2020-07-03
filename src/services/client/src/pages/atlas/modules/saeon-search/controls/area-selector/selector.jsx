1ecdff94a19fd7ff07180134273fe13ded6d74e0
Zach Smith <zach@saeon.ac.za>
2020-07-03 15:14:43 +0200

    Slight cosmetic changes to the atlas, necessary since there are people in the wild starting to use it (#26)

--- src/services/client/src/pages/atlas/modules/saeon-search/controls/area-selector/selector.jsx
+++ src/services/client/src/pages/atlas/modules/saeon-search/controls/area-selector/selector.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { IconButton, Tooltip } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
 import { Gesture as GestureIcon, CropSquare as SquareIcon } from '@material-ui/icons'
 import { Vector as VectorLayer } from 'ol/layer'
 import { Vector as VectorSource } from 'ol/source'
@@ -28,83 +28,83 @@ export default ({ updateForm, selectPolygonActive, selectRectActive, proxy, onDr
     }
   }, [])
   return (
-    <>
-      <div>
-        <Tooltip placement="right" title="Selected rectangle">
-          <IconButton
-            style={{ padding: 0 }}
-            color={selectRectActive ? 'secondary' : 'default'}
-            aria-label="select-rectangle"
-            onClick={() => {
-              proxy.removeInteraction(draw)
-              if (!selectRectActive) {
-                const source = new VectorSource({ wrapX: false })
-                const layer = new VectorLayer({
-                  id: `${nanoid()}-drawLayer`,
-                  title: 'Draw layer',
-                  source,
-                })
-                draw = new Draw({
-                  source: source,
-                  type: 'Circle',
-                  geometryFunction: createBox(),
-                })
-                proxy.addInteraction(draw)
-                draw.on('drawend', event => {
-                  const feat = event.feature
-                  const geometry = feat.getGeometry()
-                  onDrawEnd(geometry)
-                })
-                proxy.addLayer(layer)
-              }
-              updateForm({
-                selectRectActive: !selectRectActive,
-                selectPolygonActive: false,
+    <Grid container spacing={4}>
+      <Grid item xs={12} sm={6}>
+        <Button
+          fullWidth
+          variant="contained"
+          disableElevation
+          color={selectRectActive ? 'secondary' : 'default'}
+          onClick={() => {
+            proxy.removeInteraction(draw)
+            if (!selectRectActive) {
+              const source = new VectorSource({ wrapX: false })
+              const layer = new VectorLayer({
+                id: `${nanoid()}-drawLayer`,
+                title: 'Draw layer',
+                source,
               })
-            }}
-          >
-            <SquareIcon fontSize="default" />
-          </IconButton>
-        </Tooltip>
-      </div>
-      <div style={{ marginTop: 16 }}>
-        <Tooltip placement="right" title="Select polygon">
-          <IconButton
-            onClick={() => {
-              proxy.removeInteraction(draw)
-              if (!selectPolygonActive) {
-                const source = new VectorSource({ wrapX: false })
-                const layer = new VectorLayer({
-                  id: `${nanoid()}-drawLayer`,
-                  title: 'Draw layer',
-                  source,
-                })
-                draw = new Draw({
-                  source: source,
-                  type: 'Polygon',
-                  freehand: true,
-                })
-                proxy.addInteraction(draw)
-                draw.on('drawend', event => {
-                  const feat = event.feature
-                  const geometry = feat.getGeometry()
-                  onDrawEnd(geometry)
-                })
-                proxy.addLayer(layer)
-              }
-              updateForm({
-                selectPolygonActive: !selectPolygonActive,
-                selectRectActive: false,
+              draw = new Draw({
+                source: source,
+                type: 'Circle',
+                geometryFunction: createBox(),
               })
-            }}
-            style={{ padding: 0 }}
-            color={selectPolygonActive ? 'secondary' : 'default'}
-            aria-label="select-polygon"
-          >
-            <GestureIcon fontSize="default" />
-          </IconButton>
-        </Tooltip>
-      </div>
-    </>
+              proxy.addInteraction(draw)
+              draw.on('drawend', event => {
+                const feat = event.feature
+                const geometry = feat.getGeometry()
+                onDrawEnd(geometry)
+              })
+              proxy.addLayer(layer)
+            }
+            updateForm({
+              selectRectActive: !selectRectActive,
+              selectPolygonActive: false,
+            })
+          }}
+          startIcon={<SquareIcon fontSize="default" />}
+        >
+          Select square extent
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Button
+          fullWidth
+          variant="contained"
+          disableElevation
+          color={selectPolygonActive ? 'secondary' : 'default'}
+          onClick={() => {
+            proxy.removeInteraction(draw)
+            if (!selectPolygonActive) {
+              const source = new VectorSource({ wrapX: false })
+              const layer = new VectorLayer({
+                id: `${nanoid()}-drawLayer`,
+                title: 'Draw layer',
+                source,
+              })
+              draw = new Draw({
+                source: source,
+                type: 'Polygon',
+                freehand: true,
+              })
+              proxy.addInteraction(draw)
+              draw.on('drawend', event => {
+                const feat = event.feature
+                const geometry = feat.getGeometry()
+                onDrawEnd(geometry)
+              })
+              proxy.addLayer(layer)
+            }
+            updateForm({
+              selectPolygonActive: !selectPolygonActive,
+              selectRectActive: false,
+            })
+          }}
+          startIcon={<GestureIcon fontSize="default" />}
+        >
+          Select freeform extent
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
