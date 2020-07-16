edf295802c781020afb88fa29ef92c553fbd989c
Steven <steven@saeon.ac.za>
2020-07-16 14:04:26 +0200

    Fixed a lot of sizing/spacing issues of citation-dialog. dialog is looking much better. There is a potential issue if a citation is very short causing the citation text to appear far right instead of centered. This is only an issue for very short text and likely wouldn't appear for real citations. copy to clipboard button to be fixed

--- src/services/client/src/components/citation-dialog/citations.js
+++ src/services/client/src/components/citation-dialog/citations.js
@@ -14,15 +14,17 @@ export default json => {
   const resourceDescription = resourceType.resourceTypeGeneral
 
   return {
-    APA: `${author}. (${publicationYear}). ${title} [${resourceDescription}]. Retrieved from ${url}`, //https://guides.lib.umich.edu/c.php?g=439304&p=2993299
+    APA:
+      `${author}. (${publicationYear}). ${title} [${resourceDescription}]. ` +
+      (DOI ? `doi: ${DOI}` : `Retrieved from ${url}`), //https://guides.lib.umich.edu/c.php?g=439304&p=2993299 https://blog.apastyle.org/apastyle/2013/12/how-to-cite-a-data-set-in-apa-style.html
     Harvard:
       `${author} ${publicationYear}, ${title}, ${resourceDescription}, ${publisher}, ` +
       (DOI ? `doi: ${DOI}` : `viewed ${dateViewed}, <${url}>`), //https://libraryguides.vu.edu.au/c.php?g=386501&p=4347840
-    MLA: `mla cite`,
-    Vancouver: `vancouver cite`,
-    Chicago: `chicago cite`,
-    IEEE: `ieee cite`,
-    BibTeX: `bibtex cite`,
-    RIS: `ris cite`,
+    MLA: `MLA citation in development`,
+    Vancouver: `Vancouver citation in development`,
+    Chicago: `Chicago citation in development`,
+    IEEE: `IEEE citation in development`,
+    BibTeX: `BibTeX citation in development`,
+    RIS: `RIS citation in development`,
   }
 }
