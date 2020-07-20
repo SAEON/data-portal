3f6756983d5f08bf6cbe951b8e03930d5ffadc71
Zach Smith <zach@saeon.ac.za>
2020-07-20 14:33:26 +0200

    Added back xy logging

--- src/services/client/src/app.jsx
+++ src/services/client/src/app.jsx
@@ -5,35 +5,38 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import { DEFAULT_ERROR, DEFAULT_WARNING, DEFAULT_INFO, DEFAULT_SUCCESS } from './config'
 import { ApolloProvider } from '@apollo/client'
 import ErrorBoundary from './modules/error-boundary'
+import EventBoundary from './components/event-boundary'
 import AuthProvider from './modules/provider-auth'
 import FeedbackProvider from './modules/provider-feedback'
 import Layout from './modules/layout'
 import theme from './theme'
 
 export default ({ link }) => (
-  <ApolloProvider
-    client={
-      new ApolloClient({
-        cache: new InMemoryCache(),
-        link,
-      })
-    }
-  >
-    <CssBaseline>
-      <ThemeProvider theme={theme}>
-        <ErrorBoundary>
-          <FeedbackProvider
-            defaultError={DEFAULT_ERROR}
-            defaultWarning={DEFAULT_WARNING}
-            defaultInfo={DEFAULT_INFO}
-            defaultSuccess={DEFAULT_SUCCESS}
-          >
-            <AuthProvider>
-              <Layout />
-            </AuthProvider>
-          </FeedbackProvider>
-        </ErrorBoundary>
-      </ThemeProvider>
-    </CssBaseline>
-  </ApolloProvider>
+  <EventBoundary>
+    <ApolloProvider
+      client={
+        new ApolloClient({
+          cache: new InMemoryCache(),
+          link,
+        })
+      }
+    >
+      <CssBaseline>
+        <ThemeProvider theme={theme}>
+          <ErrorBoundary>
+            <FeedbackProvider
+              defaultError={DEFAULT_ERROR}
+              defaultWarning={DEFAULT_WARNING}
+              defaultInfo={DEFAULT_INFO}
+              defaultSuccess={DEFAULT_SUCCESS}
+            >
+              <AuthProvider>
+                <Layout />
+              </AuthProvider>
+            </FeedbackProvider>
+          </ErrorBoundary>
+        </ThemeProvider>
+      </CssBaseline>
+    </ApolloProvider>
+  </EventBoundary>
 )
