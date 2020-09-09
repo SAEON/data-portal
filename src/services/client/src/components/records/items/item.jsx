e8b5ca482452b37861a3d0c833dd972fcb34e166
Zach Smith <zach@saeon.ac.za>
2020-09-09 14:21:59 +0200

    Fixed word wrap bug

--- src/services/client/src/components/records/items/item.jsx
+++ src/services/client/src/components/records/items/item.jsx
@@ -163,7 +163,14 @@ export default ({
               </CardContent>
 
               {/* Description */}
-              <CardContent>{descriptions?.[0]?.description || 'No description'}</CardContent>
+              <CardContent>
+                <Typography
+                  style={{ whiteSpace: 'break-spaces', wordBreak: 'break-word' }}
+                  variant="body2"
+                >
+                  {descriptions?.[0]?.description || 'No description'}
+                </Typography>
+              </CardContent>
 
               <CardContent>
                 {DOI ? (
