0628391b51125932b218a0aec44efce7828d424c
Zach Smith <zach@saeon.ac.za>
2020-09-18 10:13:25 +0200

    Staging current changes. Bumping major (@saeon/snap-menus)

--- src/packages/snap-menus/rollup.config.js
+++ src/packages/snap-menus/rollup.config.js
@@ -1,20 +1,39 @@
 import commonjs from '@rollup/plugin-commonjs'
 import babel from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss'
 
 export default {
   input: ['src/index.js', 'src/provider.jsx'],
   output: [
     {
-      // exports: 'auto',
+      exports: 'auto',
       dir: 'dist',
       format: 'cjs',
     },
   ],
   plugins: [
+    nodeResolve(),
+    postcss({
+      plugins: []
+    }),
     babel({
       babelHelpers: 'bundled',
       exclude: 'node_modules/**',
+      presets: [
+        [
+          '@babel/env',
+          {
+            debug: false,
+            modules: false,
+            targets: {
+              esmodules: true,
+            },
+          },
+        ],
+        ['@babel/preset-react'],
+      ],
     }),
     commonjs(),
     json({
