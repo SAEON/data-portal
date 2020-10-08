374e5b79433a7b9979fb0b552cd36a40c5933fac
Zach Smith <zach@saeon.ac.za>
2020-10-08 09:18:31 +0200

    Tuned the ES index configuration and search queries to allow for more easily boosting individual fields - currently the DOI field is highly boosted

--- src/services/api/src/graphql/resolvers/types/catalogue/dsl/_multi-match.js
+++ src/services/api/src/graphql/resolvers/types/catalogue/dsl/_multi-match.js
@@ -1,19 +1,77 @@
-const fields = [
-  'titles.title',
-  'descriptions.description',
-  'creators.name',
-  'subtitle',
-  'contributors.name',
-  'subjects.subject',
-  'creators.name',
-  'identifier.identifier',
-]
+const fields = {
+  'titles.title': 3,
+  'descriptions.description': 2,
+  'creators.name': 2,
+  subtitle: 1,
+  'contributors.name': 2,
+  'subjects.subject': 1,
+  'identifier.identifier.raw_lowercase': 10,
+}
 
+/**
+ * Using many match queries allows for configuring boost,
+ * whereas a multi_match query that will always have higher
+ * scores since the scores are calculated across many fields
+ */
 export default txt => ({
-  multi_match: {
-    query: txt,
-    fields: fields,
-    type: 'best_fields',
-    fuzziness: 'AUTO',
+  bool: {
+    should: [
+      {
+        match: {
+          'titles.title': {
+            query: txt,
+            boost: fields['titles.title'],
+          },
+        },
+      },
+      {
+        match: {
+          'descriptions.description': {
+            query: txt,
+            boost: fields['descriptions.description'],
+          },
+        },
+      },
+      {
+        match: {
+          'creators.name': {
+            query: txt,
+            boost: fields['creators.name'],
+          },
+        },
+      },
+      {
+        match: {
+          subtitle: {
+            query: txt,
+            boost: fields.subtitle,
+          },
+        },
+      },
+      {
+        match: {
+          'contributors.name': {
+            query: txt,
+            boost: fields['contributors.name'],
+          },
+        },
+      },
+      {
+        match: {
+          'subjects.subject': {
+            query: txt,
+            boost: fields['subjects.subject'],
+          },
+        },
+      },
+      {
+        match: {
+          'identifier.identifier.raw_lowercase': {
+            query: txt,
+            boost: fields['identifier.identifier.raw_lowercase'],
+          },
+        },
+      },
+    ],
   },
 })
