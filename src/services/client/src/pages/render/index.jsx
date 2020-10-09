a73b43a4770e0176ae28d74f6e8c04330f580fdf
Zach Smith <zach@saeon.ac.za>
2020-10-09 15:34:16 +0200

    Fixed UI bugs

--- src/services/client/src/pages/render/index.jsx
+++ src/services/client/src/pages/render/index.jsx
@@ -44,9 +44,5 @@ export default ({ location }) => {
     })
   )
 
-  return (
-    <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
-      <Component {...props} />
-    </div>
-  )
+  return <Component {...props} />
 }
