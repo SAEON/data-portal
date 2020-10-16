7fee434469a3afb8709988d6a3ff24e9d988d044
StevenSAEON <steven@saeon.ac.za>
2020-10-16 11:05:13 +0200

    partially fixed bugs. Moving code locally once again

--- src/services/api/src/index.js
+++ src/services/api/src/index.js
@@ -10,7 +10,7 @@ import zlib from 'zlib'
 import configureApolloServer from './graphql/index.js'
 import proxy from 'koa-proxies'
 import { configure as configureElasticsearch } from './elasticsearch/index.js'
-import { NODE_ENV, PORT, GQL_PROVIDER, HTTP_PROXY, FORCE_DB_RESET } from './config.js'
+import { NODE_ENV, PORT, GQL_PROVIDER, HTTP_PROXY } from './config.js'
 import clientSession from './middleware/client-session.js'
 import { setupDb } from './postgis/index.js'
 
@@ -26,7 +26,8 @@ if (!NODE_ENV || !['production', 'development'].includes(NODE_ENV)) {
 // Configure Elasticsearch
 await configureElasticsearch()
 
-  setupDb()
+//bring back FORCE_PG_RESET **
+setupDb()
 
 /**
  * Setup HTTP server (Koa)
