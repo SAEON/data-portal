6670b130efa63568ebf84ebea27eef6ad429d644
Zach Smith <zach@saeon.ac.za>
2020-11-06 10:24:30 +0200

    Added back data-viz work in progres to this branch

--- src/services/api/src/graphql/resolvers/types/catalogue-record/index.js
+++ src/services/api/src/graphql/resolvers/types/catalogue-record/index.js
@@ -3,6 +3,6 @@ const _import = p => import(p).then(({ default: fn }) => fn)
 export default {
   id: async self => self._id,
   metadata: async self => self,
-  // dataId: await _import('./data-id/index.js'),
+  dataId: await _import('./data-id/index.js'),
   citation: await _import('./_citation.js'),
 }
