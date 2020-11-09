9db49245cd7114431a7ada32cb0df4de42401fd1
Zach Smith <zach@saeon.ac.za>
2020-11-09 13:54:17 +0200

    Updated the client to poll API to determine if PostGIS DB is prepared instead of using subscriptions

--- src/services/api/src/graphql/resolvers/types/browser-client/_databook.js
+++ src/services/api/src/graphql/resolvers/types/browser-client/_databook.js
@@ -1,10 +1,15 @@
 import mongodb from 'mongodb'
 const { ObjectID } = mongodb
+import hash from 'object-hash'
 
 export default async (self, args, ctx) => {
-  console.log('im the bc')
   const { id } = args
   const { Databooks } = await ctx.mongo.collections
-  const result = await Databooks.findOne({ _id: ObjectID(id) })
-  return result
+  const doc = await Databooks.findOne({ _id: ObjectID(id) })
+  return doc
+    ? {
+        id: hash(doc.tables),
+        doc,
+      }
+    : undefined
 }
--- src/services/client/src/pages/databook/index.jsx
+++ src/services/client/src/pages/databook/index.jsx
@@ -10,33 +10,97 @@ import 'ace-builds/src-noconflict/theme-sqlserver'
 import 'ace-builds/src-noconflict/ext-language_tools'
 import { Toolbar } from '@material-ui/core'
 
+const POLLING_INTERVAL = 1000
+
 export default ({ id }) => {
   const classes = useStyles()
 
   return (
     /**
      * Get the Mongo doc describing the databook
+     * Keep polling until all the tables are ready
      */
     <WithGqlQuery
       QUERY={gql`
         query($id: ID!) {
           browserClient {
-            findDatabook(id: $id)
+            databook(id: $id) {
+              id
+              doc
+            }
           }
         }
       `}
       variables={{ id }}
     >
-      {({ error, loading, data }) => {
+      {({ error, loading, data, startPolling, stopPolling }) => {
         if (error) {
-          throw new Error(`Error loading databook ${id}`)
+          throw new Error(`Error loading databook ${id}. ${error}`)
         }
 
         if (loading) {
           return <Loading />
         }
 
-        return 'hi'
+        const { doc: databook } = data.browserClient.databook
+        const { tables } = databook
+
+        let tablesReady = 0
+        let tablesNotReady = 0
+        Object.entries(tables).forEach(([, { ready }]) => {
+          if (ready) {
+            tablesReady += 1
+          } else {
+            tablesNotReady += 1
+          }
+        })
+
+        const ready = tablesNotReady > 0 ? false : true
+
+        if (ready) {
+          stopPolling()
+        } else {
+          startPolling(POLLING_INTERVAL)
+        }
+
+        return !ready ? (
+          `Loaded ${tablesReady} of ${Object.entries(tables).length} tables. Please wait...`
+        ) : (
+          <div className={clsx(classes.root)} style={{ marginTop: 48 }}>
+            <SplitPane minSize={100} defaultSize={250} split="vertical">
+              <div>List of tables</div>
+              <div>
+                <SplitPane primary="first" split="vertical" defaultSize={1000}>
+                  <SplitPane split="horizontal" minSize={400}>
+                    <div className={clsx(classes.root)}>
+                      <Toolbar variant="dense" className={clsx(classes.toolbar)} />
+                      <AceEditor
+                        setOptions={{
+                          enableBasicAutocompletion: true,
+                          enableLiveAutocompletion: true,
+                          enableSnippets: true,
+                        }}
+                        tabSize={2}
+                        placeholder="Select * from ..."
+                        scrollMargin={[0]}
+                        showPrintMargin={false}
+                        height="calc(100% - 48px)"
+                        width="100%"
+                        mode="sql"
+                        theme="sql-server"
+                        onChange={() => console.log('hi')}
+                        name="UNIQUE_ID_OF_DIV"
+                        editorProps={{ $blockScrolling: true }}
+                      />
+                    </div>
+                    <div>{JSON.stringify(data)}</div>
+                  </SplitPane>
+                  <div>Dashboard builder</div>
+                </SplitPane>
+              </div>
+            </SplitPane>
+          </div>
+        )
       }}
     </WithGqlQuery>
   )
