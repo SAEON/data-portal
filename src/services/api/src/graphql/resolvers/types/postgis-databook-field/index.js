371e832669d88f488805d3c3fd3b31fe87cae772
StevenSAEON <steven@saeon.ac.za>
2020-11-16 23:59:01 +0200

    debugging databook field id issue

--- src/services/api/src/graphql/resolvers/types/postgis-databook-field/index.js
+++ src/services/api/src/graphql/resolvers/types/postgis-databook-field/index.js
@@ -2,9 +2,6 @@ import hash from 'object-hash'
 const _import = p => import(p).then(({ default: fn }) => fn)
 
 export default {
-  id: async self => {
-    console.log('weird self', self)
-    return hash(self)
-  },
+  id: async self => hash(self),
   updateFieldName: await _import('./_update-field-name.js'),
 }
