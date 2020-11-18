d17c918c6cf6f6f854b65b9ee1a9dbe151b2552b
Zach Smith <zach@saeon.ac.za>
2020-11-18 14:47:38 +0200

    Docker deployment attempt to get the docker cli exposed to api

--- src/services/client/src/pages/databook/postgis/data-explorer/results/index.jsx
+++ src/services/client/src/pages/databook/postgis/data-explorer/results/index.jsx
@@ -1,13 +1,20 @@
-import { Toolbar } from '@material-ui/core'
+import { Toolbar, IconButton } from '@material-ui/core'
 import VirtualTable from '../../../../../components/virtual-table'
+import AddItemIcon from 'mdi-react/ViewGridAddIcon'
 import clsx from 'clsx'
 import useStyles from './style'
+import { useTheme } from '@material-ui/core/styles'
 
 export default ({ data }) => {
+  const theme = useTheme()
   const classes = useStyles()
   return (
     <>
-      <Toolbar variant="dense" className={clsx(classes.toolbar)} />
+      <Toolbar variant="dense" className={clsx(classes.toolbar)}>
+        <IconButton style={{ color: theme.palette.primary.light }} size="small">
+          <AddItemIcon />
+        </IconButton>
+      </Toolbar>
       <div style={{ position: 'relative', height: 'calc(100% - 48px)' }}>
         <div
           style={{
--- src/services/client/src/pages/databook/postgis/data-explorer/results/style.js
+++ src/services/client/src/pages/databook/postgis/data-explorer/results/style.js
@@ -5,5 +5,7 @@ export default makeStyles(theme => ({
   toolbar: {
     backgroundColor: fade(theme.palette.common.white, 0.5),
     borderBottom: `1px solid ${theme.palette.grey[300]}`,
+    display: 'flex',
+    justifyContent: 'flex-end',
   },
 }))
