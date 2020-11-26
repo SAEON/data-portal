09f56558bce31a25b4823c2779c993bca28573fa
Zach Smith <zach@saeon.ac.za>
2020-11-26 12:15:26 +0200

    testing webcrypto api shim for .dvn environments

--- src/services/client/webpack.config.js
+++ src/services/client/webpack.config.js
@@ -91,7 +91,7 @@ module.exports = () => {
             chunks: 'all',
             minChunks: 1,
             reuseExistingChunk: true,
-            test: /[\\/]node_modules[\\/](react|react-dom|@material-ui|ol|@apollo|react-beautiful-dnd|@mapbox|core-js|graphql)/,
+            test: /[\\/]node_modules[\\/](react|react-dom|@material-ui|ol|@apollo|react-beautiful-dnd|@mapbox|core-js|graphql|webcrypto-shim)/,
             name: module =>
               `npm.${module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]}`,
           },
