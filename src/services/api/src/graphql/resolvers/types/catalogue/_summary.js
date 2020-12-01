8b503dd66e51daca341fd51afc22eae9e1989147
Zach Smith <zach@saeon.ac.za>
2020-12-01 16:13:52 +0200

    POC refactoring of the Elasticsearch DSL to support filtering a field array by a subfield in those arrays items - the trick seems to be by using 'sub documents', which is specified in the elasticsearch mapping settings

--- src/services/api/src/graphql/resolvers/types/catalogue/_summary.js
+++ src/services/api/src/graphql/resolvers/types/catalogue/_summary.js
@@ -10,6 +10,7 @@ export default async (_, args, ctx) => {
     identifiers = undefined,
     filterByDois: dois = undefined,
     filterByTerms: terms = undefined,
+    path = undefined,
     limit: size,
   } = args
 
@@ -17,24 +18,56 @@ export default async (_, args, ctx) => {
   const dsl = {
     size: 0,
     aggs: Object.fromEntries(
-      fields.map(field => [
-        field,
-        {
-          terms: {
-            field,
-            size,
-            order,
+      fields.map(({ field, filter }) => {
+        const dsl = {
+          nested: {
+            path,
           },
-        },
-      ])
+          aggs: {
+            [field]: {
+              aggs: {
+                [field]: {
+                  terms: {
+                    field,
+                    size,
+                    order,
+                  },
+                },
+              },
+            },
+          },
+        }
+
+        if (filter) {
+          const { field: filterField, values: filterValues } = filter
+          dsl.aggs[field].filter = {
+            terms: {
+              [filterField]: filterValues,
+            },
+          }
+        } else {
+          dsl.aggs[field].filter = {
+            exists: {
+              field,
+            },
+          }
+        }
+
+        return [field, dsl]
+      })
     ),
   }
+
+  console.log(JSON.stringify(dsl))
 
   const result = await catalogue.query(
     buildDsl({ dsl, ids, dois, text, terms, identifiers, extent, isAggregation: true })
   )
 
-  return Object.entries(result.aggregations).map(([name, { buckets }]) => ({
-    [name]: buckets,
-  }))
+  return Object.entries(result.aggregations).map(([name, result]) => {
+    console.log(result[name][name])
+    return {
+      [name]: result[name][name].buckets,
+    }
+  })
 }
--- src/services/api/src/graphql/schema/datacite.graphql/index.js
+++ src/services/api/src/graphql/schema/datacite.graphql/index.js
@@ -7,15 +7,13 @@ const CITATION_LOCALES_URI = 'https://citation.crosscite.org/locales/'
 const CITATION_LOCALES_PATH = `${CATALOGUE_API_TEMP_DIRECTORY}/datacite-citation-locales.json`
 
 const dataciteCitationStyles = `
-enum CitationStyle {
-  ${await fetchContent({ cachePath: CITATION_STYLES_PATH, contentUri: CITATION_STYLES_URI })}
-}
-`
+  enum CitationStyle {
+    ${await fetchContent({ cachePath: CITATION_STYLES_PATH, contentUri: CITATION_STYLES_URI })}
+  }`
 
 const dataciteCitationLocales = `
-enum CitationLocale {
-  ${await fetchContent({ cachePath: CITATION_LOCALES_PATH, contentUri: CITATION_LOCALES_URI })}
-}
-`
+  enum CitationLocale {
+    ${await fetchContent({ cachePath: CITATION_LOCALES_PATH, contentUri: CITATION_LOCALES_URI })}
+  }`
 
 export default `${dataciteCitationStyles} ${dataciteCitationLocales}`
