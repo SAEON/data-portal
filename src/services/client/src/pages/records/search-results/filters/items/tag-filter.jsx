416f11f78028f2e4c95aaf944e1cf2690119927c
Zach Smith <zach@saeon.ac.za>
2020-12-02 10:17:16 +0200

    Updated readme to show how to configure the filter-config

--- src/services/client/src/pages/records/search-results/filters/items/tag-filter.jsx
+++ src/services/client/src/pages/records/search-results/filters/items/tag-filter.jsx
@@ -113,7 +113,6 @@ export default ({ results, title, field, sortBy = 'key', sortOrder = 'asc', styl
                           color="primary"
                           checked={terms?.map(({ value }) => value)?.includes(key) ? true : false}
                           onChange={() => {
-                            console.log('hi', terms)
                             if (terms?.map(({ value }) => value)?.includes(key)) {
                               setGlobal({
                                 terms: terms?.filter(({ value }) => value !== key),
