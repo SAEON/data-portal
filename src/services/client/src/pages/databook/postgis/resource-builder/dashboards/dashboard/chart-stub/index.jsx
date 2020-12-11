f0250ed3d108034bc7b97e969ba5a9f301073de1
Zach Smith <zach@saeon.ac.za>
2020-12-11 09:24:38 +0200

    Slightly improved item appearance of added charts

--- src/services/client/src/pages/databook/postgis/resource-builder/dashboards/dashboard/chart-stub/index.jsx
+++ src/services/client/src/pages/databook/postgis/resource-builder/dashboards/dashboard/chart-stub/index.jsx
@@ -1,10 +1,26 @@
 import RemoveChartButton from './_remove-chart-button'
+import ChartIcon from 'mdi-react/ChartBubbleIcon'
+import { Typography, Grid } from '@material-ui/core'
+
+const itemStyle = {
+  display: 'flex',
+  margin: 'auto',
+}
 
 export default ({ chart: id, dashboard }) => {
   return (
-    <div>
-      <div key={id}>Chart: {id}</div>
-      <RemoveChartButton chartId={id} dashboard={dashboard} />
+    <div style={{ height: '100%', display: 'flex', padding: 16 }}>
+      <Grid container justify="space-around" alignItems="center">
+        <Grid item style={itemStyle}>
+          <ChartIcon />
+        </Grid>
+        <Grid item style={itemStyle}>
+          <Typography variant="overline">{id}</Typography>
+        </Grid>
+        <Grid item style={itemStyle}>
+          <RemoveChartButton chartId={id} dashboard={dashboard} />
+        </Grid>
+      </Grid>
     </div>
   )
 }
