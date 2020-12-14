0e59143646ac20d0d439bcf65e752900078b90d3
Steven van Blerk <steven@saeon.ac.za>
2020-12-15 00:44:49 +0200

    WIP. base map of map chart can now technically be retrieved from our own SA maps. ground work in place. Continents table is being mapped successfully. Now to clean up test code and use districts/municipality tables

--- src/services/client/src/components/charts/map/chart/index.jsx
+++ src/services/client/src/components/charts/map/chart/index.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
+import { context as databooksContext } from '../../../../pages/databook/context'
 import ReactEcharts from 'echarts-for-react'
+import { gql } from '@apollo/client'
 import theme from '../../../../lib/echarts-theme'
-import catalogueTheme from '../../../../theme'
-import USA from './USA.json'
+import { WithGqlQuery } from '../../../../hooks'
+import usaJson from './USA.json'
 
-require('echarts/map/json/china.json')
-// require('echarts/map/js/china.js')
-// require('echarts/map/json/china.json')
-require('./USA.json')
+var echarts = require('echarts')
+
 /*
 select 
 wkb_geometry,
@@ -18,118 +18,117 @@ limit 5
 
 //https://echarts.apache.org/en/option.html#geo
 //https://echarts.apache.org/en/tutorial.html#Use%20ECharts%20with%20webpack
-
-var echarts = require('echarts')
-
-// $.get('echarts/map/json/china.json')
-// $.get('map/json/china.json', function (chinaJson) {
-//   echarts.registerMap('china', chinaJson)
-//   var chart = echarts.init(document.getElementById('testid'))
-//   chart.setOption({
-//     series: [
-//       {
-//         type: 'map',
-//         map: 'china',
-//       },
-//     ],
-//   })
-// })
-
-const MapFunc = () => {
-  echarts.registerMap('USA', USA)
-  var myChart = echarts.init(document.getElementById('testid'))
-  myChart.setOption({
-    series: [
-      {
-        type: 'map',
-        map: 'USA',
-      },
-    ],
-  })
-  const getOption = {
-    title: {
-      text: 'title',
-      subtext: 'subtitle',
-      left: 'center',
-    },
-    series: [
-      {
-        type: 'map',
-        mapType: 'china',
-        roam: true, //allows dragging of map
-        label: {
-          normal: {
-            show: true,
-          },
-        },
-
-        data: [
-          { name: 'Alabama', value: 4822023 },
-          { name: 'Alaska', value: 731449 },
-          { name: 'Arizona', value: 6553255 },
-          { name: 'Arkansas', value: 2949131 },
-          { name: 'California', value: 38041430 },
-          { name: 'Colorado', value: 5187582 },
-          { name: 'Connecticut', value: 3590347 },
-          { name: 'Delaware', value: 917092 },
-          { name: 'District of Columbia', value: 632323 },
-          { name: 'Florida', value: 19317568 },
-          { name: 'Georgia', value: 9919945 },
-          { name: 'Hawaii', value: 1392313 },
-          { name: 'Idaho', value: 1595728 },
-          { name: 'Illinois', value: 12875255 },
-          { name: 'Indiana', value: 6537334 },
-          { name: 'Iowa', value: 3074186 },
-          { name: 'Kansas', value: 2885905 },
-          { name: 'Kentucky', value: 4380415 },
-          { name: 'Louisiana', value: 4601893 },
-          { name: 'Maine', value: 1329192 },
-          { name: 'Maryland', value: 5884563 },
-          { name: 'Massachusetts', value: 6646144 },
-          { name: 'Michigan', value: 9883360 },
-          { name: 'Minnesota', value: 5379139 },
-          { name: 'Mississippi', value: 2984926 },
-          { name: 'Missouri', value: 6021988 },
-          { name: 'Montana', value: 1005141 },
-          { name: 'Nebraska', value: 1855525 },
-          { name: 'Nevada', value: 2758931 },
-          { name: 'New Hampshire', value: 1320718 },
-          { name: 'New Jersey', value: 8864590 },
-          { name: 'New Mexico', value: 2085538 },
-          { name: 'New York', value: 19570261 },
-          { name: 'North Carolina', value: 9752073 },
-          { name: 'North Dakota', value: 699628 },
-          { name: 'Ohio', value: 11544225 },
-          { name: 'Oklahoma', value: 3814820 },
-          { name: 'Oregon', value: 3899353 },
-          { name: 'Pennsylvania', value: 12763536 },
-          { name: 'Rhode Island', value: 1050292 },
-          { name: 'South Carolina', value: 4723723 },
-          { name: 'South Dakota', value: 833354 },
-          { name: 'Tennessee', value: 6456243 },
-          { name: 'Texas', value: 26059203 },
-          { name: 'Utah', value: 2855287 },
-          { name: 'Vermont', value: 626011 },
-          { name: 'Virginia', value: 8185867 },
-          { name: 'Washington', value: 6897012 },
-          { name: 'West Virginia', value: 1855413 },
-          { name: 'Wisconsin', value: 5726398 },
-          { name: 'Wyoming', value: 576412 },
-          { name: 'Puerto Rico', value: 3667084 },
-        ],
-      },
-    ],
-  }
+/*
+TODO
+-Create materialized view that stores wkb_geometry as ST_AsText or ST_Transform
+ALTERNATIVELY test if wkb_geometry format is acceptable to echarts-for-react
+-There is probably a way of registering SA maps outside of this file and simply 
+importing it like USA.json (e.g. simply storing the JSON server-side)
+*/
+export default props => {
+  console.log('props', props)
+  const { databook } = useContext(databooksContext)
+  const sql = `select 
+  ST_AsText(wkb_geometry),
+  ST_AsEWKT(ST_AsText(wkb_geometry)),
+  ST_AsGeoJSON(wkb_geometry),
+  * 
+  from "continents-simplified"
+  where wkb_geometry IS NOT NULL`
   return (
-    <div id="testid">
-      {/* <ReactEcharts
-        theme={theme}
-        option={getOption}
-        style={{ height: '100%', width: '100%' }}
-        className="react_for_echarts"
-      /> */}
-    </div>
+    <WithGqlQuery
+      QUERY={gql`
+        query($id: ID!, $sql: String!) {
+          databook(id: $id) {
+            id
+            execute(sql: $sql)
+          }
+        }
+      `}
+      variables={{ id: databook.doc._id, sql }}
+    >
+      {({ error, loading, data }) => {
+        if (loading) {
+          return 'loading..'
+        }
+        if (error) {
+          console.log('error', error)
+          return 'error!'
+        }
+        console.log('data', data)
+        let testJSON = {
+          type: 'FeatureCollection',
+
+          features: data.databook.execute.map((entry, i) => {
+            return {
+              type: 'Feature',
+              id: i, //may need to be stringified
+              properties: { name: entry.CONTINENT },
+              geometry: JSON.parse(entry.st_asgeojson),
+              // {
+              //   type: 'MultiPolygon',
+              //   coordinates: entry.st_asewkt, //probably requires further mapping
+              //   // type: 'Geom',
+              //   // coordinates: entry.wkb_geometry,
+              // },
+            }
+          }),
+
+          // features: [
+          //   {
+          //     type: 'Feature',
+          //     id: '01',
+          //     properties: { name: 'Alabama' },
+          //     geometry: {
+          //       type: 'MultiPolygon',
+          //       coordinates: [],
+          //     },
+          //   },
+          // ],
+        }
+
+        console.log('testJSON', testJSON)
+        console.log('USA', usaJson)
+        echarts.registerMap('USA', usaJson)
+        echarts.registerMap('TEST', testJSON)
+        return (
+          <div
+            id="testid"
+            style={{
+              position: 'absolute',
+              top: 40,
+              bottom: 0,
+              left: 0,
+              right: 0,
+            }}
+          >
+            <ReactEcharts
+              theme={theme}
+              option={{
+                title: {
+                  text: 'title',
+                  subtext: 'subtitle',
+                  left: 'center',
+                },
+                series: [
+                  {
+                    type: 'map',
+                    mapType: 'TEST',
+                    roam: true, //allows dragging of map
+                    label: {
+                      normal: {
+                        show: true,
+                      },
+                    },
+                  },
+                ],
+              }}
+              style={{ height: '100%', width: '100%' }}
+              className="react_for_echarts"
+            />
+          </div>
+        )
+      }}
+    </WithGqlQuery>
   )
 }
-
-export default MapFunc
