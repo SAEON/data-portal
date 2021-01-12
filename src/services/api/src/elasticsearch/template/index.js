92fdc0103208ac73d1e6ea5eaf7c69b5a1d5f354
Zach Smith <zach@saeon.ac.za>
2021-01-12 15:38:17 +0200

    Resolved index rebuild problem on stable

--- src/services/api/src/elasticsearch/template/index.js
+++ src/services/api/src/elasticsearch/template/index.js
@@ -3,21 +3,11 @@ import mappings from './mappings.json'
 import settings from './settings.json'
 import {
   ELASTICSEARCH_ADDRESS,
-  CATALOGUE_PROXY_ADDRESS,
   CATALOGUE_API_ELASTICSEARCH_TEMPLATE_NAME,
 } from '../../config.js'
-import url from 'url'
 
-/**
- * It should only be possible to configure elasticsearch
- *   => With correct authentication TODO
- *   => If the read address (proxy) is the same as the write address
- */
+
 export default async () => {
-  if (url.parse(ELASTICSEARCH_ADDRESS).hostname !== url.parse(CATALOGUE_PROXY_ADDRESS).hostname) {
-    return 'Aborting! Read / write from separate hostnames'
-  }
-
   return fetch(
     `${ELASTICSEARCH_ADDRESS}/_index_template/${CATALOGUE_API_ELASTICSEARCH_TEMPLATE_NAME}`,
     {
