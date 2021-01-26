eb11728a21ac5ac948fb1de7034ab436d8219291
Zach Smith <zachsmith.sa@gmail.com>
2021-01-26 15:17:44 +0200

    Added cookie consent banner

--- src/services/client/src/contexts/global.jsx
+++ src/services/client/src/contexts/global.jsx
@@ -1,9 +1,13 @@
-import { createContext } from 'react'
+import { createContext, forwardRef } from 'react'
 import QuickForm from '@saeon/quick-form'
 import getUriState from '../lib/fns/get-uri-state'
+import { Button } from '@material-ui/core'
+import { useTheme } from '@material-ui/core/styles'
 import Loading from '../components/loading'
 import { gql } from '@apollo/client'
 import WithGqlQuery from '../hooks/with-gql-query'
+import CookieConsent from 'react-cookie-consent'
+import { Typography } from '@material-ui/core'
 
 export const context = createContext()
 
@@ -34,49 +38,72 @@ const FromSavedSearch = ({ id, children }) =>
   )
 
 export default ({ children }) => {
+  const theme = useTheme()
   const { search, text = undefined, referrer = undefined } = getUriState()
 
   return (
-    <FromSavedSearch id={search}>
-      {search => (
-        <QuickForm
-          referrer={referrer || undefined}
-          text={text || search?.text || undefined}
-          extent={search?.extent || undefined}
-          terms={search?.terms || []}
-          ids={search?.ids || []}
-          dois={search?.dois || []}
-          selectedIds={search?.selectedIds || []}
-        >
-          {(setGlobal, global) => {
-            return (
-              <context.Provider
-                value={{
-                  global,
-                  setGlobal: (obj, clearHistory = false, cb) => {
-                    setGlobal(
-                      Object.assign(
-                        clearHistory
-                          ? {
-                              layers: [],
-                              text: undefined,
-                              extent: undefined,
-                              terms: [],
-                            }
-                          : {},
-                        obj
+    <>
+      <CookieConsent
+        style={{ background: theme.palette.primary.light }}
+        ButtonComponent={forwardRef((props, ref) => {
+          return (
+            <Button
+              {...Object.fromEntries(Object.entries(props).filter(([k]) => k !== 'style'))}
+              color="secondary"
+              variant="outlined"
+              style={{ marginRight: theme.spacing(1) }}
+              ref={ref}
+            >
+              Okay
+            </Button>
+          )
+        })}
+        location="bottom"
+      >
+        <Typography>This website uses cookies to enhance the user experience.</Typography>
+      </CookieConsent>
+
+      <FromSavedSearch id={search}>
+        {search => (
+          <QuickForm
+            referrer={referrer || undefined}
+            text={text || search?.text || undefined}
+            extent={search?.extent || undefined}
+            terms={search?.terms || []}
+            ids={search?.ids || []}
+            dois={search?.dois || []}
+            selectedIds={search?.selectedIds || []}
+          >
+            {(setGlobal, global) => {
+              return (
+                <context.Provider
+                  value={{
+                    global,
+                    setGlobal: (obj, clearHistory = false, cb) => {
+                      setGlobal(
+                        Object.assign(
+                          clearHistory
+                            ? {
+                                layers: [],
+                                text: undefined,
+                                extent: undefined,
+                                terms: [],
+                              }
+                            : {},
+                          obj
+                        )
                       )
-                    )
-                    if (cb) cb()
-                  },
-                }}
-              >
-                {children}
-              </context.Provider>
-            )
-          }}
-        </QuickForm>
-      )}
-    </FromSavedSearch>
+                      if (cb) cb()
+                    },
+                  }}
+                >
+                  {children}
+                </context.Provider>
+              )
+            }}
+          </QuickForm>
+        )}
+      </FromSavedSearch>
+    </>
   )
 }
