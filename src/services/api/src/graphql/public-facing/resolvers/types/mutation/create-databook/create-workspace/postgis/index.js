7846ecca4433cb7e7f68ed34331c342599c2a9e3
Zach Smith <zachsmith.sa@gmail.com>
2021-01-27 11:34:22 +0200

    Bumped pg docker image version. Fixed bug where ogr2ogr import failed due to permissions error

--- src/services/api/src/graphql/public-facing/resolvers/types/mutation/create-databook/create-workspace/postgis/index.js
+++ src/services/api/src/graphql/public-facing/resolvers/types/mutation/create-databook/create-workspace/postgis/index.js
@@ -2,6 +2,22 @@ import { collections } from '../../../../../../../../mongo/index.js'
 import provisionSchema from './provision-schema/index.js'
 import importGisData from './import-gis-data/index.js'
 
+/**
+ * NOTE
+ *
+ * When I designed this workflow I didn't realize that ogr2ogr
+ * would want to write to public.* tables (where the PostGIS extension
+ * is installed)
+ *
+ * If I had... I would have used the admin username/password to create
+ * the schema and then adjusted roles for a new user after the fact.
+ *
+ * Also, it might be a better idea to create a database per user than
+ * a schema. This would allow for enabling PostGIS per user, which would
+ * be helpful. However then shared tables would need to be duplicated per
+ * user
+ */
+
 export default async (ctx, { records, databookId }) => {
   const { Databooks } = await collections
   const databook = await Databooks.findOne({ _id: databookId })
@@ -12,7 +28,7 @@ export default async (ctx, { records, databookId }) => {
    * Provision other necessary tables
    * Adjust roles and search path for user
    */
-  await provisionSchema(ctx, databook)
+  const cleanupAfterOgr2Ogr = await provisionSchema(ctx, databook)
 
   /**
    * Download the GIS data (immutableResource)
@@ -21,4 +37,10 @@ export default async (ctx, { records, databookId }) => {
   for (const record of records) {
     await importGisData(ctx, databook, { ...record._source })
   }
+
+  /**
+   * Revoke write access to PostGIS extension
+   * (GDAL ogr2ogr requires this when importing shapefiles)
+   */
+  await cleanupAfterOgr2Ogr()
 }
