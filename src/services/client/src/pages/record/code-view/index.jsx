a6cc0e166a613e5ae5314077b12f8d57875730b2
Zach Smith <zachsmith.sa@gmail.com>
2021-02-10 09:42:02 +0200

    Minor styling adjustments

--- src/services/client/src/pages/record/code-view/index.jsx
+++ src/services/client/src/pages/record/code-view/index.jsx
@@ -1,14 +1,25 @@
 import Row from '../_row'
 import { Grid, Fade } from '@material-ui/core'
+import { useTheme } from '@material-ui/core/styles'
 
-export default ({ codeView, json }) => (
-  <Fade key="1" in={codeView}>
-    <Grid container direction="column" justify="space-evenly" alignItems="stretch">
-      <Row style={{ width: '100%' }} title={'Metadata Source JSON'}>
-        <pre style={{ whiteSpace: 'break-spaces', wordBreak: 'break-word' }}>
-          {JSON.stringify(json, null, 2)}
-        </pre>
-      </Row>
-    </Grid>
-  </Fade>
-)
+export default ({ codeView, json }) => {
+  const theme = useTheme()
+
+  return (
+    <Fade key="1" in={codeView}>
+      <Grid
+        style={{ padding: theme.spacing(4) }}
+        container
+        direction="column"
+        justify="space-evenly"
+        alignItems="stretch"
+      >
+        <Row title={'Metadata Source JSON'}>
+          <pre style={{ whiteSpace: 'break-spaces', wordBreak: 'break-word' }}>
+            {JSON.stringify(json, null, 2)}
+          </pre>
+        </Row>
+      </Grid>
+    </Fade>
+  )
+}
