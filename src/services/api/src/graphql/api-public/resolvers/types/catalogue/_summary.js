cd018d3884a9834bc5dd267b2ae5f0d3ed7bb454
Zach Smith <zachsmith.sa@gmail.com>
2021-02-16 16:04:16 +0200

    Forgot to save file

--- src/services/api/src/graphql/api-public/resolvers/types/catalogue/_summary.js
+++ src/services/api/src/graphql/api-public/resolvers/types/catalogue/_summary.js
@@ -11,7 +11,7 @@ import buildDsl from './dsl/index.js'
  * DON'T CHANGE
  */
 const FILTER_DELIMITER = '__FILTERED_BY__'
-const FILTER_VALUE_DELIMTER = ''
+const FILTER_VALUE_DELIMITER = ''
 
 /**
  * NOTE
@@ -51,9 +51,11 @@ export default async (_, args, ctx) => {
       fields.map(({ field, filter, path }) => {
         const { field: filterField, values: filterValues } = filter || {}
         const aggregationName = filterValues
-          ? `${field}${FILTER_DELIMITER}${filterValues.join(FILTER_VALUE_DELIMTER)}`
+          ? `${field}${FILTER_DELIMITER}${filterValues.join(FILTER_VALUE_DELIMITER)}`
           : field
         const dsl = {}
+
+        // TODO 'or' clause for when 'includeIfMissingField' is true
 
         /**
          * The path indicates that sub-documents are being searched
