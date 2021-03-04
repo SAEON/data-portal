b5c9f13675ed0f504f24b0734cd04a529ddefe38
Zach Smith <zach@saeon.ac.za>
2021-03-04 09:42:52 +0200

    refactored catalogue search to be a class (vs code suggestion)

--- src/services/api/src/lib/catalogue.js
+++ src/services/api/src/lib/catalogue.js
@@ -3,39 +3,41 @@ import fetch from 'node-fetch'
 
 const errorHeader = `${packageJson.name} v${packageJson.version}`
 
-export default function Catalogue({ dslAddress = null, index = null }) {
-  if (!dslAddress || !index) {
-    throw new Error(
-      `${errorHeader}. class ElasticCatalogue. ERROR: Incorrect options param passed to constructor`
-    )
-  }
-  this.httpClient = fetch
-  this.dslAddress = dslAddress
-  this.index = index
-}
-
-Catalogue.prototype.query = async function (dsl) {
-  if (!dsl) {
-    throw new Error(`${errorHeader}. DSL object is required to query the Elasticsearch instance`)
-  } else {
-    dsl = typeof dsl === 'string' ? JSON.parse(dsl) : dsl
+export default class Catalogue {
+  constructor({ dslAddress = null, index = null }) {
+    if (!dslAddress || !index) {
+      throw new Error(
+        `${errorHeader}. class ElasticCatalogue. ERROR: Incorrect options param passed to constructor`
+      )
+    }
+    this.httpClient = fetch
+    this.dslAddress = dslAddress
+    this.index = index
   }
 
-  try {
-    return await this.httpClient(`${this.dslAddress}/${this.index}/_search`, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(dsl),
-    }).then(res => res.json())
-  } catch (error) {
-    throw new Error(
-      `${errorHeader}. class ElasticCatalogue. ERROR: query failed with DSL body ${JSON.stringify(
-        dsl
-      )}. ${error}`
-    )
+  async query(dsl) {
+    if (!dsl) {
+      throw new Error(`${errorHeader}. DSL object is required to query the Elasticsearch instance`)
+    } else {
+      dsl = typeof dsl === 'string' ? JSON.parse(dsl) : dsl
+    }
+
+    try {
+      return await this.httpClient(`${this.dslAddress}/${this.index}/_search`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(dsl),
+      }).then(res => res.json())
+    } catch (error) {
+      throw new Error(
+        `${errorHeader}. class ElasticCatalogue. ERROR: query failed with DSL body ${JSON.stringify(
+          dsl
+        )}. ${error}`
+      )
+    }
   }
 }
