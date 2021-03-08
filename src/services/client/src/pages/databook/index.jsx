e3d48fe48a1af9afdc7de6585ffa6d4ca0887ed9
Zach Smith <zach@saeon.ac.za>
2021-03-08 12:17:34 +0200

    wip started on refactoring databook

--- src/services/client/src/pages/databook/index.jsx
+++ src/services/client/src/pages/databook/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import DatabookContextProvider from './context'
-import { gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 import { setShareLink } from '../../hooks/use-share-link'
 import WithGqlQuery from '../../hooks/with-gql-query'
 import LoadingDatabook from './loading'
@@ -23,149 +23,149 @@ export default ({ id }) => {
     uri: `${CATALOGUE_CLIENT_ADDRESS}/render/databook?id=${id}`,
     params: false,
   })
-  return (
-    <WithGqlQuery
-      QUERY={gql`
-        query($id: ID!) {
-          databook(id: $id) {
+
+  const { error, loading, data, startPolling, stopPolling } = useQuery(
+    gql`
+      query($id: ID!) {
+        databook(id: $id) {
+          id
+          doc
+          charts {
             id
-            doc
-            charts {
-              id
-              title
-            }
-            filters {
-              id
-              name
-              columnFiltered
-              values
-              sql
-            }
+            title
+          }
+          filters {
+            id
+            name
+            columnFiltered
+            values
+            sql
           }
         }
-      `}
-      variables={{ id }}
+      }
+    `,
+    {
+      variables: {
+        id,
+      },
+    }
+  )
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    if (error.message.toUpperCase() === 'FORBIDDEN') {
+      return error.message // Otherwise user is flashed an error message before being redirected to login
+    } else {
+      throw new Error(`Error loading databook ${id}. ${error}`)
+    }
+  }
+
+  const { doc: databook } = data.databook
+  const { tables } = databook
+
+  let tablesReady = 0
+  let tablesNotReady = 0
+  let errors = []
+  Object.entries(tables).forEach(([tableName, { ready, error }]) => {
+    if (ready) {
+      tablesReady++
+    } else {
+      if (error) {
+        errors.push({ tableName, error })
+      } else {
+        tablesNotReady++
+      }
+    }
+  })
+
+  const ready = tablesNotReady > 0 ? false : true
+
+  /**
+   * Get the Mongo doc describing the databook
+   * Keep polling until all the tables are ready
+   */
+  if (ready) {
+    stopPolling()
+  } else {
+    startPolling(POLLING_INTERVAL)
+  }
+
+  if (!ready) {
+    return <LoadingDatabook tables={tables} tablesReady={tablesReady} />
+  }
+
+  return (
+    <div
+      className={clsx(
+        classes.layout,
+        {
+          [classes.pushDown]: !window.location.pathname.includes('/render'),
+        },
+        classes.bg
+      )}
     >
-      {({ error, loading, data, startPolling, stopPolling }) => {
-        if (loading) {
-          return <Loading />
-        }
-
-        if (error) {
-          if (error.message.toUpperCase() === 'FORBIDDEN') {
-            return error.message // Otherwise user is flashed an error message before being redirected to login
-          } else {
-            throw new Error(`Error loading databook ${id}. ${error}`)
-          }
-        }
-
-        const { doc: databook } = data.databook
-        const { tables } = databook
-
-        let tablesReady = 0
-        let tablesNotReady = 0
-        let errors = []
-        Object.entries(tables).forEach(([tableName, { ready, error }]) => {
-          if (ready) {
-            tablesReady++
-          } else {
-            if (error) {
-              errors.push({ tableName, error })
-            } else {
-              tablesNotReady++
-            }
-          }
-        })
-
-        const ready = tablesNotReady > 0 ? false : true
-
-        /**
-         * Get the Mongo doc describing the databook
-         * Keep polling until all the tables are ready
-         */
-        if (ready) {
-          stopPolling()
-        } else {
-          startPolling(POLLING_INTERVAL)
-        }
-
-        return ready ? (
-          <div
-            className={clsx(
-              classes.layout,
+      {errors.length ? (
+        <Box m={2}>
+          <Typography>
+            Error preparing data. Please send the message below to a system administrator (
+            {CATALOGUE_TECHNICAL_CONTACT})
+          </Typography>
+          <pre>
+            {JSON.stringify(
               {
-                [classes.pushDown]: !window.location.pathname.includes('/render'),
+                message: 'Error creating databook',
+                uri: window.location,
+                errors,
               },
-              classes.bg
+              null,
+              2
             )}
-          >
-            {errors.length ? (
-              <Box m={2}>
-                <Typography>
-                  Error preparing data. Please send the message below to a system administrator (
-                  {CATALOGUE_TECHNICAL_CONTACT})
-                </Typography>
-                <pre>
-                  {JSON.stringify(
-                    {
-                      message: 'Error creating databook',
-                      uri: window.location,
-                      errors,
-                    },
-                    null,
-                    2
-                  )}
-                </pre>
-              </Box>
-            ) : (
-              <WithGqlQuery
-                QUERY={gql`
-                  query($id: ID!) {
-                    databook(id: $id) {
+          </pre>
+        </Box>
+      ) : (
+        <WithGqlQuery
+          QUERY={gql`
+            query($id: ID!) {
+              databook(id: $id) {
+                id
+                schema {
+                  id
+                  tables {
+                    id
+                    table_schema
+                    odpRecordId
+                    description
+                    fields {
                       id
-                      schema {
-                        id
-                        tables {
-                          id
-                          table_schema
-                          odpRecordId
-                          description
-                          fields {
-                            id
-                            column_name
-                            data_type
-                            ordinal_position
-                          }
-                        }
-                      }
+                      column_name
+                      data_type
+                      ordinal_position
                     }
                   }
-                `}
-                variables={{ id: data.databook.doc._id }}
-              >
-                {({ error, loading, data: schemaData }) => {
-                  if (loading) {
-                    return <Loading />
-                  }
-                  if (error) {
-                    throw error
-                  }
-                  return (
-                    <DatabookContextProvider
-                      schema={schemaData.databook.schema}
-                      databook={data.databook}
-                    >
-                      <DataExplorer />
-                    </DatabookContextProvider>
-                  )
-                }}
-              </WithGqlQuery>
-            )}
-          </div>
-        ) : (
-          <LoadingDatabook tables={tables} tablesReady={tablesReady} />
-        )
-      }}
-    </WithGqlQuery>
+                }
+              }
+            }
+          `}
+          variables={{ id: data.databook.doc._id }}
+        >
+          {({ error, loading, data: schemaData }) => {
+            if (loading) {
+              return <Loading />
+            }
+            if (error) {
+              throw error
+            }
+            return (
+              <DatabookContextProvider schema={schemaData.databook.schema} databook={data.databook}>
+                <DataExplorer />
+              </DatabookContextProvider>
+            )
+          }}
+        </WithGqlQuery>
+      )}
+    </div>
   )
 }
