51bf306f786ea921f7d8defd6c7033f45dec4681
Zach Smith <zach@saeon.ac.za>
2021-03-10 15:32:31 +0200

    Refactored GQL schema to force queries for a databook's dashboards to go through the databook entity

--- src/services/api/src/graphql/api-public/resolvers/queries/dashboards.js
+++ src/services/api/src/graphql/api-public/resolvers/queries/dashboards.js
@@ -1,8 +1,7 @@
 import mongodb from 'mongodb'
 const { ObjectID } = mongodb
 
-export default async (_, args, ctx) => {
-  const { databookId } = args
-  const { Dashboards } = await ctx.mongo.collections
-  return await Dashboards.find({ databookId: ObjectID(databookId) }).toArray()
+export default async (_, { ids }, ctx) => {
+  const { findDashboards } = ctx.mongo.dataFinders
+  return await findDashboards({ _id: { $in: ids.map(id => ObjectID(id)) } })
 }
