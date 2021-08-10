31cd511bc7d9e722fe80f1e17a88e5e42ace7949
Zach Smith <zachsmith.sa@gmail.com>
2021-08-10 13:49:28 +0200

    Trying npm force for npm 7 deploy

--- scripts/install-dependencies.js
+++ scripts/install-dependencies.js
@@ -20,7 +20,7 @@ apply({ PATH: PACKAGES_PATH, script: NPM_SCRIPTS.install })
  * Install service dependencies
  */
 const SERVICES_PATH = normalize(join(__dirname, '../src'))
-apply({ PATH: SERVICES_PATH, script: NPM_SCRIPTS.install, args: ['--legacy-peer-deps'] })
+apply({ PATH: SERVICES_PATH, script: NPM_SCRIPTS.install, args: ['--force'] })
 
 /**
  * Install tool dependencies
