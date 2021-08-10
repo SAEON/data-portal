44ba0bce53c13ca965dcb25e35583c58da13098f
Zach Smith <zachsmith.sa@gmail.com>
2021-08-10 09:09:32 +0200

    Updated download proxy (for media.dirisa queries) to use node-fetch

--- src/api/src/http/download-proxy.js
+++ src/api/src/http/download-proxy.js
@@ -9,7 +9,12 @@ export default async ctx => {
   const { hostname } = new URL(uri)
 
   if (HOSTNAME_WHITELIST.includes(hostname)) {
-    ctx.body = fetch(uri)
+    const res = await fetch(uri)
+    ctx.set('Content-type', res.headers.get('last-modified'))
+    ctx.set('Content-type', res.headers.get('content-length'))
+    ctx.set('Content-type', res.headers.get('content-type'))
+    ctx.set('content-disposition', res.headers.get('content-disposition'))
+    ctx.body = res.body
   } else {
     ctx.body = `The hostname ${hostname} is potentially insecure. If you would like to download the resource anyway, navigate to ${uri} in a new tab. If you think this message is in error, please contact the SAEON curation team to whitelist this URL at ${CURATOR_CONTACT}`
   }
