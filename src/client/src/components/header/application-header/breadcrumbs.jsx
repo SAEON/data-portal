d18277f422cc1060f8ac051b3dba5336dadd90f6
Zach Smith <zachsmith.sa@gmail.com>
2021-08-10 12:08:24 +0200

    Fixed breadcrumbs for record identified via DOI

--- src/client/src/components/header/application-header/breadcrumbs.jsx
+++ src/client/src/components/header/application-header/breadcrumbs.jsx
@@ -21,7 +21,12 @@ const useStyles = makeStyles(theme => ({
 export default function IconBreadcrumbs({ routes }) {
   const classes = useStyles()
   const { pathname } = useLocation() // Trigger re-render on location changes
-  const tree = [...new Set(pathname.split('/'))].map(p => {
+
+  const _pathname = pathname.match(/\/records\/./)
+    ? ['', 'records', ...new Set(pathname.split('/records/')).filter(_ => _)]
+    : [...new Set(pathname.split('/'))]
+
+  const tree = _pathname.map(p => {
     return (
       routes.find(({ to }) => {
         to = to.replace('/', '')
