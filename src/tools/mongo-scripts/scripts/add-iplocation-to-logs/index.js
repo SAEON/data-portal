602b1294ade7a19f1d3e6859b0020f146fa494c1
Zach Smith <zachsmith.sa@gmail.com>
2021-08-23 14:37:26 +0200

    Fixed bug where first 100 IP addresses were ignored in tool to convert ip addresses to ip locations

--- src/tools/mongo-scripts/scripts/add-iplocation-to-logs/index.js
+++ src/tools/mongo-scripts/scripts/add-iplocation-to-logs/index.js
@@ -5,14 +5,16 @@ import updateLogs from './_update-logs.js'
 
 export default async () => {
   const distinctIps = await getDistinctIps()
-  let ipIterator = iterateOverArray(distinctIps)
-  while (!ipIterator.done) {
-    ipIterator = ipIterator.next()
+
+  do {
+    let ipIterator = iterateOverArray(distinctIps)
     const locations = await resolveIpLocations(ipIterator.items)
     for (const location of locations) {
       await updateLogs(location)
     }
-  }
+    ipIterator = ipIterator.next()
+  } while (!ipIterator.done)
+
   console.info('=== Script complete')
   process.exit(0)
 }
