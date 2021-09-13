3fae4425d03311cd1ee1e1fd45529b01c5e585bd
Zach Smith <zachsmith.sa@gmail.com>
2021-09-13 09:43:22 +0200

    Updated deps Updated the elasticsearch DSL editor to look nice

--- src/client/src/pages/data-lists/components/list-item/search/index.jsx
+++ src/client/src/pages/data-lists/components/list-item/search/index.jsx
@@ -3,19 +3,55 @@ import { context as listContext } from '../_context'
 import CardContent from '@material-ui/core/CardContent'
 import useTheme from '@material-ui/core/styles/useTheme'
 import Collapse from '../../../../../components/collapse'
+import AceEditor from 'react-ace'
+import 'ace-builds/webpack-resolver'
+import 'ace-builds/src-min-noconflict/mode-json'
+import 'ace-builds/src-min-noconflict/theme-vibrant_ink'
 
 export default () => {
-  const { update, search } = useContext(listContext)
+  const { update, search, id } = useContext(listContext)
   const theme = useTheme()
 
   return (
     <Collapse title="Search" subheader="Configure list search DSL object" defaultExpanded>
       <CardContent>
         <div
-          style={{ padding: theme.spacing(1), maxHeight: 400, overflow: 'auto' }}
+          style={{ padding: theme.spacing(1), height: 400, width: '100%', overflow: 'auto' }}
           contentEditable
         >
-          <pre style={{ maxHeight: 800, overflow: 'auto' }}>{JSON.stringify(search, null, 2)}</pre>
+          <AceEditor
+            height="100%"
+            width="100%"
+            name={`ace-editor-${id}`}
+            value={JSON.stringify(search, null, 2)}
+            editorProps={{ $blockScrolling: false, $blockSelectEnabled: true }}
+            mode="json"
+            theme="vibrant_ink"
+            setOptions={{
+              wrap: true,
+              enableBasicAutocompletion: false,
+              enableLiveAutocompletion: false,
+              enableSnippets: false,
+              firstLineNumber: 1,
+              newLineMode: 'unix',
+              useSoftTabs: true,
+              tabSize: 2,
+              scrollPastEnd: 8,
+              selectionStyle: 'line',
+              highlightActiveLine: true,
+              highlightSelectedWord: true,
+              cursorStyle: 'ace',
+              animatedScroll: true,
+              displayIndentGuides: true,
+              fadeFoldWidgets: true,
+              vScrollBarAlwaysVisible: false,
+              scrollSpeed: 1,
+              highlightGutterLine: true,
+              fontSize: 12,
+              fixedWidthGutter: true,
+              showPrintMargin: false,
+            }}
+          />
         </div>
       </CardContent>
     </Collapse>
