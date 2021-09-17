9a6b1d34c05ec1f611ae0201362d2ea57bbdd875
Zach Smith <zachsmith.sa@gmail.com>
2021-09-17 14:46:39 +0200

    Added react-data-table to project

--- src/clients/src/pages/data-submissions/index.jsx
+++ src/clients/src/pages/data-submissions/index.jsx
@@ -1,8 +1,8 @@
 import Container from '@material-ui/core/Container'
 import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
 import Header from './header'
 import useTheme from '@material-ui/core/styles/useTheme'
+import DataGrid, { TextEditor } from 'react-data-grid'
 
 export default () => {
   const theme = useTheme()
@@ -10,10 +10,19 @@ export default () => {
   return (
     <>
       <Header />
-      <div style={{ margin: theme.spacing(1) }} />
+      <div style={{ margin: theme.spacing(2) }} />
       <Container style={{ minHeight: 1000 }}>
         <Card variant="outlined" style={{ backgroundColor: theme.backgroundColor }}>
-          <CardContent>Data submissions page</CardContent>
+          <DataGrid
+            columns={[
+              { key: 'id', name: 'ID', editor: TextEditor },
+              { key: 'title', name: 'Title', editor: TextEditor },
+            ]}
+            rows={[
+              { id: 0, title: 'Example' },
+              { id: 1, title: 'Demo' },
+            ]}
+          />
         </Card>
       </Container>
     </>
