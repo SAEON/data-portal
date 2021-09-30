55cc982c3ba4beeff6f2975013001eda8d0488b3
Zach Smith <zachsmith.sa@gmail.com>
2021-09-30 08:41:49 +0200

    Added sorting to graphql queries for retrieving permissions

--- src/api/src/graphql/api-public/resolvers/queries/permissions.js
+++ src/api/src/graphql/api-public/resolvers/queries/permissions.js
@@ -1,4 +1,8 @@
 export default async (self, args, ctx) => {
   const { findPermissions } = await ctx.mongo.dataFinders
-  return await findPermissions({})
+  return (await findPermissions({})).sort(({ name: a }, { name: b }) => {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+  })
 }
--- src/api/src/graphql/api-public/resolvers/types/role/_permissions.js
+++ src/api/src/graphql/api-public/resolvers/types/role/_permissions.js
@@ -1,4 +1,8 @@
 export default async ({ permissions }, args, ctx) => {
   const { findPermissions } = ctx.mongo.dataFinders
-  return await findPermissions({ _id: { $in: permissions } })
+  return (await findPermissions({ _id: { $in: permissions } })).sort(({ name: a }, { name: b }) => {
+    if (a > b) return 1
+    if (a < b) return -1
+    return 0
+  })
 }
