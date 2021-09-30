b4f5ca7f0a7938a96e15a0ef43688321f4de1ea7
Zach Smith <zachsmith.sa@gmail.com>
2021-09-30 08:33:53 +0200

    Added not implemented stub for download usage

--- src/clients/src/pages/usage/header/_download.jsx
+++ src/clients/src/pages/usage/header/_download.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import DownloadIcon from 'mdi-react/DownloadIcon'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogActions from '@material-ui/core/DialogActions'
 
 export default () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <Button startIcon={<DownloadIcon size={18} />} variant="text" size="small" disableElevation>
-      Download data
-    </Button>
+    <>
+      {/* TOGGLE */}
+      <Button
+        onClick={() => setOpen(true)}
+        startIcon={<DownloadIcon size={18} />}
+        variant="text"
+        size="small"
+        disableElevation
+      >
+        Download data
+      </Button>
+
+      {/* DIALOGUE */}
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Download usage data</DialogTitle>
+        <DialogContent>Not implemented yet</DialogContent>
+        <DialogActions>
+          <Button variant="contained" size="small" disabled>
+            Download
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
