84c874466c8a80c0a9a543b31870637e330780df
Zach Smith <zachsmith.sa@gmail.com>
2021-10-08 11:16:16 +0200

    Updated the API to allow for creating metadata records

--- src/api/src/graphql/api-public/resolvers/mutations/create-metadata/index.js
+++ src/api/src/graphql/api-public/resolvers/mutations/create-metadata/index.js
@@ -1,28 +1,39 @@
 import { ObjectId } from 'mongodb'
 import fetch from 'node-fetch'
 import { ODP_API } from '../../../../../config/index.js'
+import { v4 as UUIDv4 } from 'uuid'
 
-export default async (self, { input }, ctx) => {
+export default async (self, { input, numberOfRecords = 1, institution }, ctx) => {
   const { id: currentUserId } = ctx.user.info(ctx)
   const { findUsers } = ctx.mongo.dataFinders
   const user = (await findUsers({ _id: ObjectId(currentUserId) }))[0]
   const { token_type, access_token } = user.tokenSet
-  const authorization = [token_type, access_token].join(' ')
 
-  const res = await fetch(`${ODP_API}/saeon/metadata`, {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      accept: 'application/json',
-      authorization,
-    },
-    body: JSON.stringify({
-      sid: 'asdfasd99923e4sfasdfad',
-      collection_key: 'string',
-      schema_key: 'string',
-      metadata: {},
-    }),
-  })
-
-  return []
+  return await Promise.all(
+    new Array(numberOfRecords).fill(
+      new Promise((resolve, reject) =>
+        fetch(`${ODP_API}/${institution}/metadata/`, {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+            accept: 'application/json',
+            authorization: [token_type, access_token].join(' '),
+          },
+          body: JSON.stringify({
+            sid: UUIDv4(),
+            ...input,
+          }),
+        })
+          .then(res => res.json())
+          .then(json => {
+            if (json.detail) {
+              throw new Error(`ERROR creating metadata records: ${json.detail}`)
+            } else {
+              return resolve(json)
+            }
+          })
+          .catch(error => reject(error))
+      )
+    )
+  )
 }
