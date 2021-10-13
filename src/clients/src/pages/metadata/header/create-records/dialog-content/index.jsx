99611cf05c61bee4dfbd0ad91b401ac1ecd9b267
Zach Smith <zachsmith.sa@gmail.com>
2021-10-13 10:47:46 +0200

    Added some nice UI touches

--- src/clients/src/pages/metadata/header/create-records/dialog-content/index.jsx
+++ src/clients/src/pages/metadata/header/create-records/dialog-content/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { context as dialogContext } from '../context'
 import DialogContent from '@material-ui/core/DialogContent'
 import Collapse from '@material-ui/core/Collapse'
+import Fade from '@material-ui/core/Fade'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import useTheme from '@material-ui/core/styles/useTheme'
 import Form from './form'
@@ -22,31 +23,39 @@ const Content = ({
   return (
     <DialogContent dividers={true}>
       <Collapse in={createMetadataLoading} key="show-loading">
-        <div style={{ textAlign: 'center', marginBottom: theme.spacing(4) }}>
-          <Typography variant="overline" style={{ display: 'block' }}>
-            Please wait...
-          </Typography>
-          <Typography
-            variant="overline"
-            style={{ display: 'block', marginBottom: theme.spacing(4) }}
-          >
-            This can take a few minutes
-          </Typography>
-          <CircularProgress size={60} />
-        </div>
+        <span>
+          <Fade in={createMetadataLoading} key="show-loading-fade">
+            <div style={{ textAlign: 'center', marginBottom: theme.spacing(4) }}>
+              <Typography variant="overline" style={{ display: 'block' }}>
+                Please wait...
+              </Typography>
+              <Typography
+                variant="overline"
+                style={{ display: 'block', marginBottom: theme.spacing(4) }}
+              >
+                This can take a few minutes
+              </Typography>
+              <CircularProgress size={60} />
+            </div>
+          </Fade>
+        </span>
       </Collapse>
 
       <Collapse in={!createMetadataLoading} key="show-form">
         <span>
-          <Form
-            form={form}
-            setForm={setForm}
-            schemaVersions={schemaVersions}
-            institutionOptions={institutionOptions}
-            schemaOptions={schemaOptions}
-            loadingCollections={loadingCollections}
-            licenseOptions={licenseOptions}
-          />
+          <Fade in={!createMetadataLoading} key="show-form-fade">
+            <span>
+              <Form
+                form={form}
+                setForm={setForm}
+                schemaVersions={schemaVersions}
+                institutionOptions={institutionOptions}
+                schemaOptions={schemaOptions}
+                loadingCollections={loadingCollections}
+                licenseOptions={licenseOptions}
+              />
+            </span>
+          </Fade>
         </span>
       </Collapse>
     </DialogContent>
