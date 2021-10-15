32cf501f86ceba7860135e1da21610fe4370f436
Zach Smith <zachsmith.sa@gmail.com>
2021-10-15 15:16:35 +0200

    Further fixing ui bugs from mui upgrade

--- src/clients/src/pages/record/header/index.jsx
+++ src/clients/src/pages/record/header/index.jsx
@@ -1,5 +1,4 @@
 import { useContext } from 'react'
-import Grid from '@mui/material/Grid'
 import Hidden from '@mui/material/Hidden'
 import DataDownloadButton from '../../../components/data-download'
 import { context as authorizationContext } from '../../../contexts/authorization'
@@ -9,7 +8,7 @@ import CitationButton from '../../../components/citation-dialogue'
 import CodeViewButton from './_code-view-button'
 import Title from './_title'
 import FancyHeader from '../../../components/toolbar-header'
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles'
 import UpdateDataFormat from '../../../components/update-data-format'
 import Tooltip from '@mui/material/Tooltip'
 import UpdateIcon from 'mdi-react/DatabaseEditIcon'
@@ -22,39 +21,36 @@ export default ({ codeView, toggleCodeView, _source }) => {
   const canUpdateIndex = hasPermission('es-index:update')
 
   return (
-    <FancyHeader style={{ paddingLeft: theme.spacing(2), paddingRight: theme.spacing(2) }}>
-      <Grid container spacing={2} justifyContent="flex-end">
-        <Grid item xs style={{ alignSelf: 'center' }}>
-          <Title {..._source} />
-        </Grid>
+    <FancyHeader
+      style={{ paddingLeft: theme.spacing(2), paddingRight: theme.spacing(2), display: 'flex' }}
+    >
+      <Title {..._source} />
+      <div style={{ marginRight: 'auto' }} />
 
-        <PreviewAtlasButton {..._source} buttonSize="medium" />
+      <PreviewAtlasButton {..._source} buttonSize="medium" />
 
-        {canUpdateIndex && (
-          <UpdateDataFormat
-            {..._source}
-            Button={({ open, setOpen }) => (
-              <Tooltip placement="top" title="Update this record to include format information">
-                <IconButton style={{ cursor: 'pointer' }} onClick={() => setOpen(!open)} size="large">
-                  <UpdateIcon size={22} />
-                </IconButton>
-              </Tooltip>
-            )}
-          />
-        )}
-
-        <Hidden smDown>
-          <CreateDatabookButton {..._source} buttonSize="medium" />
-          {isAuthenticated && (
-            <CodeViewButton codeView={codeView} toggleCodeView={toggleCodeView} />
+      {canUpdateIndex && (
+        <UpdateDataFormat
+          {..._source}
+          Button={({ open, setOpen }) => (
+            <Tooltip placement="top" title="Update this record to include format information">
+              <IconButton style={{ cursor: 'pointer' }} onClick={() => setOpen(!open)} size="large">
+                <UpdateIcon size={22} />
+              </IconButton>
+            </Tooltip>
           )}
-          <CitationButton {..._source} buttonSize="medium" />
-        </Hidden>
+        />
+      )}
 
-        <Hidden smDown>
-          <DataDownloadButton buttonProps={{ color: 'primary' }} {..._source} />
-        </Hidden>
-      </Grid>
+      <Hidden smDown>
+        <CreateDatabookButton {..._source} buttonSize="medium" />
+        {isAuthenticated && <CodeViewButton codeView={codeView} toggleCodeView={toggleCodeView} />}
+        <CitationButton {..._source} buttonSize="medium" />
+      </Hidden>
+
+      <Hidden smDown>
+        <DataDownloadButton buttonProps={{ color: 'primary' }} {..._source} />
+      </Hidden>
     </FancyHeader>
-  );
+  )
 }
