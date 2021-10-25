9c44bd20f5407db17d58f2f2f37f0b420a082802
Zach Smith <zachsmith.sa@gmail.com>
2021-10-25 10:30:34 +0200

    Reverted schema changes

--- src/api/src/graphql/api-public/resolvers/queries/schemas/index.js
+++ src/api/src/graphql/api-public/resolvers/queries/schemas/index.js
@@ -1,4 +1,4 @@
 // TODO - should be dynamic. Not sure there is an endpoint for this yet
 export default async (self, args, ctx) => {
-  return ['saeon-datacite-4-3', 'datacite4-saeon', 'iso19115-saeon-profile', 'iso19115-saeon']
+  return ['saeon-datacite-4-3', 'iso19115-saeon-profile']
 }
