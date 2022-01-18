e4f0f71ea445fe9feb6adedf7c6d8cc3b7200203
Zach Smith <zachsmith.sa@gmail.com>
2022-01-18 08:07:33 +0200

    Finished converting all styling to use emotion (I think)

--- src/clients/src/components/fancy-button/index.jsx
+++ src/clients/src/components/fancy-button/index.jsx
@@ -1,100 +1,100 @@
-import useStyles from './style'
-import ButtonBase from '@mui/material/ButtonBase'
-import { Link } from 'react-router-dom'
-import Typography from '@mui/material/Typography'
-import clsx from 'clsx'
 import { styled, alpha } from '@mui/material/styles'
+import ButtonBase from '@mui/material/ButtonBase'
+import Typography from '@mui/material/Typography'
+import { Link } from 'react-router-dom'
 
-const Root = styled('div')(({ theme }) => ({
+const Button = styled(ButtonBase)(({ theme }) => ({
   display: 'flex',
   width: '100%',
   height: '100%',
+  position: 'relative',
+  [theme.breakpoints.down('sm')]: {
+    width: '100% !important',
+    height: 100,
+  },
+  '& .MuiTypography-root': {
+    outline: '2px solid transparent',
+  },
+  '&:hover, &.Mui-focusVisible': {
+    zIndex: 1,
+    '& .MuiFancyButtonBackdrop-root': {
+      opacity: 0.15,
+    },
+    '& .MuiFancyButtonMarked-root': {
+      opacity: 0,
+    },
+    '& .MuiTypography-root': {
+      outline: '2px solid currentColor',
+    },
+  },
+  transition: theme.transitions.create(['border']),
   border: `1px solid ${alpha(theme.palette.common.white, 0.5)}`,
   '&:hover': {
-    border: `1px solid ${alpha(theme.palette.primary.main, 0.1)}`,
+    border: `2px solid ${alpha(theme.palette.common.white, 0.1)}`,
   },
-  transition: theme.transitions.create('border'),
 }))
 
-const Span = styled('span')({})
+const Image = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: theme.palette.common.white,
+}))
+
+const Backdrop = styled('span')(({ theme }) => ({
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create('opacity'),
+}))
+
+const Mark = styled('span')(({ theme }) => ({
+  height: 2,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: 'absolute',
+  bottom: -2,
+  left: 'calc(50% - 9px)',
+  transition: theme.transitions.create('opacity'),
+}))
 
 export default ({ title, disabled = false, href = undefined, to = '/records', style = {} }) => {
-  const classes = useStyles()
-
   return (
     <div style={Object.assign({ width: '100%', height: '100%' }, style)}>
-      <Root className={classes.root}>
-        <ButtonBase
-          disabled={disabled}
-          component={href ? undefined : Link}
-          href={href}
-          to={href ? undefined : to}
-          focusRipple
-          sx={{
-            width: '100%',
-            height: '100%',
-            ...(disabled
-              ? {}
-              : {
-                  position: 'relative',
-                }),
-          }}
-          className={clsx({
-            [classes.button]: !disabled,
-          })}
-        >
-          <Span
-            sx={
-              disabled
-                ? {}
-                : {
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                    backgroundColor: theme => theme.palette.common.black,
-                    transition: theme => theme.transitions.create('opacity'),
-                  }
-            }
-            className={clsx({
-              [classes.bg]: !disabled,
-            })}
-          />
-          <Span
-            sx={
-              disabled
-                ? {}
-                : {
-                    position: 'absolute',
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0,
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    color: theme => theme.palette.common.white,
-                  }
-            }
+      <Button
+        component={href ? undefined : Link}
+        href={href}
+        to={href ? undefined : to}
+        disabled={disabled}
+        focusRipple
+      >
+        <Backdrop className="MuiFancyButtonBackdrop-root" />
+        <Image>
+          <Typography
+            component="span"
+            variant="subtitle1"
+            color="inherit"
+            sx={{
+              position: 'relative',
+              p: 4,
+              pt: 2,
+              pb: theme => `calc(${theme.spacing(1)} + 6px)`,
+            }}
           >
-            <Typography
-              component="span"
-              variant="overline"
-              color="inherit"
-              sx={theme => ({
-                transition: theme.transitions.create(['border', 'color']),
-                padding: `${theme.spacing(2)} ${theme.spacing(4)}`,
-                textAlign: 'center',
-                margin: theme.spacing(2),
-              })}
-              className={classes.title}
-            >
-              {title}
-            </Typography>
-          </Span>
-        </ButtonBase>
-      </Root>
+            {title}
+            <Mark className="MuiFancyButtonMarked-root" />
+          </Typography>
+        </Image>
+      </Button>
     </div>
   )
 }
--- src/clients/src/pages/login/index.jsx
+++ src/clients/src/pages/login/index.jsx
@@ -13,7 +13,7 @@ export default () => {
       <main style={{ height: '90vh', display: 'flex', alignItems: 'center' }} id="login">
         <Container style={{ display: 'flex', justifyContent: 'center' }}>
           <FancyButton
-            style={{ width: 250, height: 250 }}
+            style={{ width: 150, height: 100 }}
             title="Log in"
             href={`${PUBLIC_HTTP_ADDRESS}/login?redirect=${redirect}`}
           />
