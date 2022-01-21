1dfe40238733e3866113ba1943606d3b9bb6655c
Zach Smith <zachsmith.sa@gmail.com>
2022-01-21 16:36:47 +0200

    Fixed citation formatting. Refactoring away inline styles

---
+++ src/clients/src/components/html-tags/index.jsx
@@ -0,0 +1,14 @@
+import { styled } from '@mui/material/styles'
+
+export const Div = styled('div')({})
+
+export const Span = styled('span')({})
+
+export const Pre = styled('pre')(({ theme }) => ({
+  backgroundColor: theme.palette.grey[100],
+  borderRadius: theme.shape.borderRadius,
+  border: `1px solid ${theme.palette.grey[200]}`,
+  whiteSpace: 'pre-wrap',
+  wordBreak: 'break-all',
+  padding: theme.spacing(1),
+}))
