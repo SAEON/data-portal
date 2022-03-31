ba1d352a2e42d8c05a2a2769f72380804130d29d
Zach Smith <zachsmith.sa@gmail.com>
2022-03-31 12:35:49 +0200

    Updated deprecated elasticsearch rule to support Marc's QGIS plugin with the new index

--- src/proxy/src/config.js
+++ src/proxy/src/config.js
@@ -29,4 +29,4 @@ export const ELASTICSEARCH_8_1_ADDRESS =
 
 export const TERRESTRIS_ADDRESS = 'https://ows.terrestris.de'
 
-export const ALLOWED_ES_INDICES = ['saeon-odp-search', 'saeon-odp-catalogue-search']
+export const ALLOWED_ES_INDICES = ['saeon-odp-search', 'saeon-odp-catalogue-search', 'nccis-qgis-index']
