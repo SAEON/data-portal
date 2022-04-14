c6bcefe4eb70d85cd19a23a56bb938e396712653
Zach Smith <zachsmith.sa@gmail.com>
2022-04-14 12:21:34 +0200

    Updated application banner links to be limited to the visible logo area

--- src/clients/src/components/header/application-banner/index.jsx
+++ src/clients/src/components/header/application-banner/index.jsx
@@ -1,62 +1,75 @@
 import Typography from '@mui/material/Typography'
 import Toolbar_ from './toolbar'
 import Hidden from '@mui/material/Hidden'
+import { A, Img, Header, Div } from '../../html-tags'
 
 export const IMAGE_HEIGHT = 48
 
 export const Toolbar = Toolbar_
 
-const aProps = {
-  style: { flexBasis: 0, flexGrow: 1, margin: 8, display: 'flex' },
-  target: '_blank',
-  rel: 'noreferrer'
-}
-
 export default ({ title }) => {
   return (
     <Toolbar_>
       {/* SAEON LOGO */}
-      <Hidden lgDown>
-        <a {...aProps} href="http://www.saeon.ac.za/">
-          <img
-            style={{ maxHeight: IMAGE_HEIGHT, width: 'auto' }}
-            src="/saeon-logo.png"
-            alt="SAEON logo"
-          />
-        </a>
+      <Hidden mdDown>
+        <Div
+          sx={{
+            flexBasis: 0,
+            flexGrow: 1,
+            display: 'flex',
+            // height: theme => `${IMAGE_HEIGHT + parseInt(theme.spacing(1).replace('px', ''), 10)}px`,
+          }}
+        >
+          <A target="_blank" sx={{ display: 'flex' }} href="http://www.saeon.ac.za/">
+            <Img
+              style={{ maxHeight: IMAGE_HEIGHT, width: 'auto' }}
+              src="/saeon-logo.png"
+              alt="SAEON logo"
+            />
+          </A>
+        </Div>
       </Hidden>
 
       {/* TITLE */}
-      <header
-        style={{
+      <Header
+        sx={{
           display: 'flex',
           flexBasis: 0,
           flexGrow: 1,
           textAlign: 'center',
           minHeight: IMAGE_HEIGHT,
           alignItems: 'center',
-          justifyContent: 'center'
+          justifyContent: 'center',
         }}
       >
         <Typography color="textPrimary" variant="h5" variantMapping={{ h5: 'h1' }}>
           {title || 'SAEON Data'}
         </Typography>
-      </header>
+      </Header>
 
       {/* SARVA LOGO */}
-      <Hidden lgDown>
-        <a {...aProps} href="http://sarva.saeon.ac.za/">
-          <img
-            style={{
-              maxHeight: IMAGE_HEIGHT,
-              width: 'auto',
-              display: 'block',
-              marginLeft: 'auto'
-            }}
-            src="/sarva-logo-cropped.png"
-            alt="SARVA logo"
-          />
-        </a>
+      <Hidden mdDown>
+        <Div
+          sx={{
+            flexBasis: 0,
+            flexGrow: 1,
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <A sx={{ display: 'flex' }} target="_blank" href="http://sarva.saeon.ac.za/">
+            <Img
+              style={{
+                maxHeight: IMAGE_HEIGHT,
+                width: 'auto',
+                display: 'block',
+                marginLeft: 'auto',
+              }}
+              src="/sarva-logo-cropped.png"
+              alt="SARVA logo"
+            />
+          </A>
+        </Div>
       </Hidden>
     </Toolbar_>
   )
--- src/clients/src/components/html-tags/index.jsx
+++ src/clients/src/components/html-tags/index.jsx
@@ -4,11 +4,29 @@ export const Div = styled('div')({})
 
 export const Span = styled('span')({})
 
+export const Main = styled('main')({})
+
+export const Header = styled('header')({})
+
+export const Img = styled('img')({})
+
+export const A = styled('a')({})
+
+export const Table = styled('table')({})
+export const Thead = styled('thead')({})
+export const Tbody = styled('tbody')({})
+export const Tfoot = styled('tfoot')({})
+export const Tr = styled('tr')({})
+export const Th = styled('th')({})
+export const Td = styled('td')({})
+
+export const Code = styled('code')({})
+
 export const Pre = styled('pre')(({ theme }) => ({
   backgroundColor: theme.palette.grey[100],
   borderRadius: theme.shape.borderRadius,
   border: `1px solid ${theme.palette.grey[200]}`,
   whiteSpace: 'pre-wrap',
   wordBreak: 'break-all',
-  padding: theme.spacing(1)
+  padding: theme.spacing(1),
 }))
