fcd91680a7f548fa5821d28357423ec720bc4a07
Zach Smith <zachsmith.sa@gmail.com>
2022-10-27 05:35:45 +0200

    Added error log

--- src/api/src/integrations/saeon-odp/metadata/extract/_transform.js
+++ src/api/src/integrations/saeon-odp/metadata/extract/_transform.js
@@ -1,52 +1,59 @@
 import { parseDates, parseSpatial, parseImmutableResource } from '../transform/index.js'
 
-export default data =>
- { 
-  return data
-    .map(
-      (
-        {
-          id,
-          sid, // eslint-disable-line
-          doi,
-          institution_key: institution,
-          collection_key: collection,
-          project_keys: projects,
-          schema_key: schema,
-          metadata,
-          published,
-        },
-        i
-      ) => {
-        try {
-          if (!published) {
-            return undefined
-          }
+export default data => {
+  try {
+    return (
+      data
+        .map(
+          (
+            {
+              id,
+              sid, // eslint-disable-line
+              doi,
+              institution_key: institution,
+              collection_key: collection,
+              project_keys: projects,
+              schema_key: schema,
+              metadata,
+              published,
+            },
+            i
+          ) => {
+            try {
+              if (!published) {
+                return undefined
+              }
 
-          return {
-            id,
-            doi,
-            institution,
-            collection,
-            projects,
-            schema,
-            ...Object.fromEntries(
-              Object.entries(metadata).map(([key, value]) =>
-                key === 'immutableResource'
-                  ? [key, parseImmutableResource(id, value)]
-                  : key === 'dates'
-                  ? [key, parseDates(id, value)]
-                  : key === 'geoLocations'
-                  ? [key, parseSpatial(id, value)]
-                  : [key, value]
-              )
-            ),
+              return {
+                id,
+                doi,
+                institution,
+                collection,
+                projects,
+                schema,
+                ...Object.fromEntries(
+                  Object.entries(metadata).map(([key, value]) =>
+                    key === 'immutableResource'
+                      ? [key, parseImmutableResource(id, value)]
+                      : key === 'dates'
+                      ? [key, parseDates(id, value)]
+                      : key === 'geoLocations'
+                      ? [key, parseSpatial(id, value)]
+                      : [key, value]
+                  )
+                ),
+              }
+            } catch (error) {
+              console.error('ERROR processing record from the ODP', id, error.message)
+              return undefined
+            }
           }
-        } catch (error) {
-          console.error('ERROR processing record from the ODP', id, error.message)
-          return undefined
-        }
-      }
+        )
+        // Filter away published === false
+        .filter(_ => _)
     )
-    // Filter away published === false
-    .filter(_ => _)}
+  } catch (error) {
+    console.error('data', data)
+    throw error
+  }
+}
