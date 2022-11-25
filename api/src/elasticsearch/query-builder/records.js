03dcd14792e8867614d14dfe6d75ab38bfa0ff51
Zach Smith <zachsmith.sa@gmail.com>
2022-11-25 09:05:38 +0200

    Updated restart policy tohave a shorter delay. And updated error handling

--- api/src/elasticsearch/query-builder/records.js
+++ api/src/elasticsearch/query-builder/records.js
@@ -12,84 +12,88 @@ import { ELASTICSEARCH_CATALOGUE_INDEX, LOG_QUERY_DETAILS } from '../../config/i
  */
 
 export default async ({ ctx, args }) => {
-  const { elastic } = ctx
+  try {
+    const { elastic } = ctx
 
-  const {
-    size = 10000,
-    ids = undefined,
-    dois = undefined,
-    text = undefined,
-    extent = undefined,
-    terms = undefined,
-    identifiers = undefined,
-    filter = {},
-  } = args
+    const {
+      size = 10000,
+      ids = undefined,
+      dois = undefined,
+      text = undefined,
+      extent = undefined,
+      terms = undefined,
+      identifiers = undefined,
+      filter = {},
+    } = args
 
-  let { before = undefined, after = undefined } = args
+    let { before = undefined, after = undefined } = args
 
-  if (size < 1 || size > 10000) {
-    throw new Error('Size param must be between 1 and 200 (maximum page size)')
-  }
+    if (size < 1 || size > 10000) {
+      throw new Error('Size param must be between 1 and 200 (maximum page size)')
+    }
 
-  if (before && after) {
-    throw new Error('Please specify either no cursor, a "before" or an "after" cursor (not both)')
-  }
+    if (before && after) {
+      throw new Error('Please specify either no cursor, a "before" or an "after" cursor (not both)')
+    }
 
-  if (before) {
-    before = JSON.parse(Buffer.from(before, 'base64').toString('utf8'))
-  }
+    if (before) {
+      before = JSON.parse(Buffer.from(before, 'base64').toString('utf8'))
+    }
 
-  if (after) {
-    after = JSON.parse(Buffer.from(after, 'base64').toString('utf8'))
-  }
+    if (after) {
+      after = JSON.parse(Buffer.from(after, 'base64').toString('utf8'))
+    }
 
-  const dsl = {
-    size,
-    query: {
-      bool: {
-        must: [],
-        filter: [],
+    const dsl = {
+      size,
+      query: {
+        bool: {
+          must: [],
+          filter: [],
+        },
       },
-    },
-    sort: [
-      { _score: before ? 'asc' : 'desc' },
-      {
-        'id.raw': before ? 'asc' : 'desc',
-      },
-    ],
-  }
+      sort: [
+        { _score: before ? 'asc' : 'desc' },
+        {
+          'id.raw': before ? 'asc' : 'desc',
+        },
+      ],
+    }
 
-  if (before || after) {
-    const cursor = before || after
-    dsl.search_after = [cursor.score || 0, cursor.id]
-  }
+    if (before || after) {
+      const cursor = before || after
+      dsl.search_after = [cursor.score || 0, cursor.id]
+    }
 
-  const body = buildDsl({ dsl, ids, dois, text, terms, extent, identifiers, filter })
+    const body = buildDsl({ dsl, ids, dois, text, terms, extent, identifiers, filter })
 
-  if (LOG_QUERY_DETAILS) {
-    console.info('ES DSL', JSON.stringify(body, null, 2))
-  }
+    if (LOG_QUERY_DETAILS) {
+      console.info('ES DSL', JSON.stringify(body, null, 2))
+    }
 
-  const data = await elastic.query({
-    index: ELASTICSEARCH_CATALOGUE_INDEX,
-    body,
-  })
+    const data = await elastic.query({
+      index: ELASTICSEARCH_CATALOGUE_INDEX,
+      body,
+    })
 
-  if (data.error) {
-    return new Error(`${JSON.stringify(data.error, null, 2)}`)
-  }
+    if (data.error) {
+      throw new Error(`${JSON.stringify(data.error, null, 2)}`)
+    }
 
-  const totalCount = data.hits.total.value
+    const totalCount = data.hits.total.value
 
-  return {
-    pageSize: size >= totalCount ? totalCount : size,
-    totalCount,
-    hasPreviousPage: undefined,
-    hasNextPage: undefined,
-    _firstResult:
-      before === undefined ? data.hits.hits[0] : data.hits.hits[data.hits.hits.length - 1],
-    _lastResult:
-      before === undefined ? data.hits.hits[data.hits.hits.length - 1] : data.hits.hits[0],
-    hits: data.hits.hits,
+    return {
+      pageSize: size >= totalCount ? totalCount : size,
+      totalCount,
+      hasPreviousPage: undefined,
+      hasNextPage: undefined,
+      _firstResult:
+        before === undefined ? data.hits.hits[0] : data.hits.hits[data.hits.hits.length - 1],
+      _lastResult:
+        before === undefined ? data.hits.hits[data.hits.hits.length - 1] : data.hits.hits[0],
+      hits: data.hits.hits,
+    }
+  } catch (error) {
+    return new Error(`${JSON.stringify(error, null, 2)}`)
   }
 }
