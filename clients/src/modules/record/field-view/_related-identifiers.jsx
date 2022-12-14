f6e29450f168d5e6fe2b6e5f0a3e424ddfe98b78
Zach Smith <zachsmith.sa@gmail.com>
2022-12-14 06:26:26 +0200

    Updated related identifiers as pr Leo's request (#70)

--- clients/src/modules/record/field-view/_related-identifiers.jsx
+++ clients/src/modules/record/field-view/_related-identifiers.jsx
@@ -3,13 +3,23 @@ import Typography from '@mui/material/Typography'
 import Link from '@mui/material/Link'
 import { Div, B } from '../../../components/html-tags'
 
-export default ({ relatedIdentifiers }) => (
-  <Row title="Related Resources">
-    {relatedIdentifiers.map((lr, i) => (
-      <Div key={`related-resource${i}`}>
-        <Typography variant="body2">
-          <B>{`${lr?.relationType?.split(/(?=[A-Z])/).join(' ') || '(Unknown relationship)'}`}</B>{' '}
-          {lr?.relatedIdentifierType.toUpperCase() === 'DOI' ? (
+const EXCLUDE_RELATIONSHIPS = ['HasMetadata']
+
+export default ({ relatedIdentifiers }) => {
+  relatedIdentifiers = relatedIdentifiers.filter(
+    ({ relationType: t }) => !EXCLUDE_RELATIONSHIPS.includes(t)
+  )
+
+  if (relatedIdentifiers.length < 1) {
+    return undefined
+  }
+
+  return (
+    <Row title="Related Resources">
+      {relatedIdentifiers.map((lr, i) => (
+        <Div key={`related-resource${i}`}>
+          <Typography variant="body2">
+            <B>{`${lr?.relationType?.split(/(?=[A-Z])/).join(' ') || '(Unknown relationship)'}`}</B>{' '}
             <Link
               target="_blank"
               rel="noopener noreferrer"
@@ -17,11 +27,9 @@ export default ({ relatedIdentifiers }) => (
                 lr.relatedIdentifier
               }`}
             >{`${lr.relatedIdentifier || 'Missing/incorrect metadata'}`}</Link>
-          ) : (
-            lr.relatedIdentifier || 'Missing/incorrect metadata'
-          )}
-        </Typography>
-      </Div>
-    ))}
-  </Row>
-)
+          </Typography>
+        </Div>
+      ))}
+    </Row>
+  )
+}
