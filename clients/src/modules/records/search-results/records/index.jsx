b360d6010a02b6fff4445c71c300bd6efc4c05f2
Zach Smith <zachsmith.sa@gmail.com>
2023-01-17 15:34:27 +0200

    Refactoring to avoid unecessary loops while rendering search page (#70 #66)

--- clients/src/modules/records/search-results/records/index.jsx
+++ clients/src/modules/records/search-results/records/index.jsx
@@ -1,49 +1,61 @@
+import { useContext } from 'react'
+import { context as authContext } from '../../../../contexts/authorization'
 import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 import Record, { Wrapper, Paper } from './record'
 
-export default ({ results }) => (
-  <Grid
-    container
-    item
-    xs={12}
-    sx={theme => ({
-      mb: 0,
-      [theme.breakpoints.up('md')]: {
-        mb: theme.spacing(2),
-      },
-    })}
-  >
-    {/* Results */}
-    {Boolean(results.length) &&
-      results.map(({ metadata, downloadCount }, i, arr) => {
-        const { _source } = metadata
-        return (
-          <Wrapper downloadCount={downloadCount} key={_source.id} {..._source}>
-            <Record i={i} length={arr.length} {..._source} />
-          </Wrapper>
-        )
-      })}
+export default ({ results }) => {
+  const { hasPermission } = useContext(authContext)
+  const canViewDownloadCount = hasPermission('record:view-downloadCount')
 
-    {/* NO Results */}
-    {!results.length && (
-      <Grid
-        item
-        xs={12}
-        sx={{
-          '&:hover': {
-            backgroundColor: theme => theme.palette.common.white,
-          },
-        }}
-        component={Paper}
-      >
-        <Typography
-          sx={{ m: theme => theme.spacing(5), display: 'block', textAlign: 'center' }}
-          variant="overline"
+  return (
+    <Grid
+      container
+      item
+      xs={12}
+      sx={theme => ({
+        mb: 0,
+        [theme.breakpoints.up('md')]: {
+          mb: theme.spacing(2),
+        },
+      })}
+    >
+      {/* Results */}
+      {Boolean(results.length) &&
+        results.map(({ metadata, downloadCount }, i, arr) => {
+          const { _source } = metadata
+          return (
+            <Wrapper
+              canViewDownloadCount={canViewDownloadCount}
+              downloadCount={downloadCount}
+              key={_source.id}
+              {..._source}
+            >
+              <Record i={i} length={arr.length} {..._source} />
+            </Wrapper>
+          )
+        })}
+
+      {/* NO Results */}
+      {!results.length && (
+        <Grid
+          item
+          xs={12}
+          sx={{
+            '&:hover': {
+              backgroundColor: theme => theme.palette.common.white,
+            },
+          }}
+          component={Paper}
         >
-          No results found
-        </Typography>
-      </Grid>
-    )}
-  </Grid>
-)
+          <Typography
+            sx={{ m: theme => theme.spacing(5), display: 'block', textAlign: 'center' }}
+            variant="overline"
+          >
+            No results found
+          </Typography>
+        </Grid>
+      )}
+    </Grid>
+  )
+}
