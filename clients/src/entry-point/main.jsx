29175a6a677b4a1974497ad73ad6a3fc656374f3
Zach Smith <zachsmith.sa@gmail.com>
2023-01-23 15:48:55 +0200

    Removed IE 11 support #70

--- clients/src/entry-point/main.jsx
+++ clients/src/entry-point/main.jsx
@@ -3,9 +3,6 @@ import 'regenerator-runtime'
 import 'typeface-roboto'
 import '../index.css'
 import '../lib/log-config'
-if (!window.crypto) window.crypto = window.msCrypto // IE 11
-import 'cross-fetch/polyfill' // IE 11
-import 'abortcontroller-polyfill/dist/polyfill-patch-fetch' // IE 11
 import { createRoot } from 'react-dom/client'
 import theme from '../theme/mui'
 import { ThemeProvider } from '@mui/material/styles'
