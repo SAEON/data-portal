56ce05c5eb737952ec055ce10a76ec380103bf16
Zach Smith <zachsmith.sa@gmail.com>
2023-01-31 09:47:56 +0200

    Updated curated list functionality to allow including formatted links in list descriptions (#75 #70)

--- clients/src/components/header/application-header/title-header.jsx
+++ clients/src/components/header/application-header/title-header.jsx
@@ -1,9 +1,55 @@
+import { useMemo } from 'react'
 import Toolbar_ from './toolbar'
 import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import { Span } from '../../html-tags'
 
 export const Toolbar = Toolbar_
 
+const parseDescription = description => {
+  const parser = new DOMParser()
+  const dom = parser.parseFromString(description, 'text/html')
+  const childNodes = dom.getElementsByTagName('body')[0].childNodes
+  const nodes = []
+  for (const [, node] of childNodes.entries()) {
+    switch (node.constructor) {
+      case HTMLAnchorElement:
+        nodes.push({
+          Component: Link,
+          props: {
+            href: node.href,
+            children: node.innerHTML,
+          },
+          el: node,
+        })
+        break
+
+      default:
+        if (node.constructor !== Text) {
+          console.warn(
+            'Unknown node type when parsing list description. Treating as Text node',
+            description,
+            node.constructor
+          )
+        }
+
+        nodes.push({
+          Component: Span,
+          props: {
+            children: node.nodeValue,
+          },
+          el: node,
+        })
+        break
+    }
+  }
+
+  return nodes
+}
+
 export default ({ description, ...props }) => {
+  const nodes = useMemo(() => parseDescription(description), [description])
+
   return (
     <Toolbar_
       sx={{
@@ -25,7 +71,9 @@ export default ({ description, ...props }) => {
           },
         })}
       >
-        {description}
+        {nodes.map(({ Component, props }, i) => (
+          <Component key={i} {...props} />
+        ))}
       </Typography>
     </Toolbar_>
   )
