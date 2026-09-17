00e50b166ad9fa2d500dfdb2506f80748572026a
dylan <d.pivo@saeon.nrf.ac.za>
2026-09-17 09:03:02 +0200

    Add detailed logging to OAuth authentication callback route

--- api/src/http/oauth-authentication-callback/index.js
+++ api/src/http/oauth-authentication-callback/index.js
@@ -1,3 +1,23 @@
 import passport from 'koa-passport'
 
-export default async (ctx, next) => passport.authenticate('oidc')(ctx, next)
+export default async (ctx, next) => {
+  console.log('[OIDC CALLBACK] Session state keys:', Object.keys(ctx.session || {}))
+  console.log('[OIDC CALLBACK] OIDC session value:', ctx.session?.['oidc:protologue.saeon.ac.za'])
+
+  return passport.authenticate('oidc', (err, user, info) => {
+    if (err || !user) {
+      console.error('[OIDC CALLBACK] Auth failed:', err || info || 'No user returned')
+      ctx.status = 401
+      ctx.body = `Authentication failed: ${err?.message || info?.message || 'Unauthorized'}`
+      return
+    }
+    return ctx.login(user, err => {
+      if (err) {
+        ctx.status = 500
+        ctx.body = err.message
+        return
+      }
+      return next()
+    })
+  })(ctx, next)
+}
