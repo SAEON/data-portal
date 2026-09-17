ac952493ee98e8adc00d9f472c90ae45c0b71fac
dylan <d.pivo@saeon.nrf.ac.za>
2026-09-17 10:45:11 +0200

    Await ctx.login in OAuth callback to trigger redirect

--- api/src/http/oauth-authentication-callback/index.js
+++ api/src/http/oauth-authentication-callback/index.js
@@ -1,23 +1,21 @@
 import passport from 'koa-passport'
 
 export default async (ctx, next) => {
-  console.log('[OIDC CALLBACK] Session state keys:', Object.keys(ctx.session || {}))
-  console.log('[OIDC CALLBACK] OIDC session value:', ctx.session?.['oidc:protologue.saeon.ac.za'])
-
-  return passport.authenticate('oidc', (err, user, info) => {
+  return passport.authenticate('oidc', async (err, user, info) => {
     if (err || !user) {
       console.error('[OIDC CALLBACK] Auth failed:', err || info || 'No user returned')
       ctx.status = 401
       ctx.body = `Authentication failed: ${err?.message || info?.message || 'Unauthorized'}`
       return
     }
-    return ctx.login(user, err => {
-      if (err) {
-        ctx.status = 500
-        ctx.body = err.message
-        return
-      }
+
+    try {
+      await ctx.login(user)
       return next()
-    })
+    } catch (loginErr) {
+      console.error('[OIDC CALLBACK] ctx.login error:', loginErr)
+      ctx.status = 500
+      ctx.body = loginErr.message
+    }
   })(ctx, next)
 }
